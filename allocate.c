0a3851a3eaf2a6921ccf9aefbfb4e5892c8921ac
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2013-10-17 16:20:24 -0400

    reindent allcoate.c

--- allocate.c
+++ allocate.c
@@ -20,68 +20,68 @@
  */
 void allocate_memory(void)
 {
-  size_t bytes;
+    size_t bytes;
 
-  double bytes_tot = 0;
+    double bytes_tot = 0;
 
-  int NTaskTimesThreads;
+    int NTaskTimesThreads;
 
-  NTaskTimesThreads = NTask;
+    NTaskTimesThreads = NTask;
 #ifdef NUM_THREADS
-  NTaskTimesThreads = NUM_THREADS * NTask;
+    NTaskTimesThreads = NUM_THREADS * NTask;
 #endif
 
-  Exportflag = (int *) mymalloc("Exportflag", NTaskTimesThreads * sizeof(int));
-  Exportindex = (int *) mymalloc("Exportindex", NTaskTimesThreads * sizeof(int));
-  Exportnodecount = (int *) mymalloc("Exportnodecount", NTaskTimesThreads * sizeof(int));
+    Exportflag = (int *) mymalloc("Exportflag", NTaskTimesThreads * sizeof(int));
+    Exportindex = (int *) mymalloc("Exportindex", NTaskTimesThreads * sizeof(int));
+    Exportnodecount = (int *) mymalloc("Exportnodecount", NTaskTimesThreads * sizeof(int));
 
-  Send_count = (int *) mymalloc("Send_count", sizeof(int) * NTask);
-  Send_offset = (int *) mymalloc("Send_offset", sizeof(int) * NTask);
-  Recv_count = (int *) mymalloc("Recv_count", sizeof(int) * NTask);
-  Recv_offset = (int *) mymalloc("Recv_offset", sizeof(int) * NTask);
+    Send_count = (int *) mymalloc("Send_count", sizeof(int) * NTask);
+    Send_offset = (int *) mymalloc("Send_offset", sizeof(int) * NTask);
+    Recv_count = (int *) mymalloc("Recv_count", sizeof(int) * NTask);
+    Recv_offset = (int *) mymalloc("Recv_offset", sizeof(int) * NTask);
 
-  ProcessedFlag = (unsigned char *) mymalloc("ProcessedFlag", bytes = All.MaxPart * sizeof(unsigned char));
-  bytes_tot += bytes;
+    ProcessedFlag = (unsigned char *) mymalloc("ProcessedFlag", bytes = All.MaxPart * sizeof(unsigned char));
+    bytes_tot += bytes;
 
-  NextActiveParticle = (int *) mymalloc("NextActiveParticle", bytes = All.MaxPart * sizeof(int));
-  bytes_tot += bytes;
+    NextActiveParticle = (int *) mymalloc("NextActiveParticle", bytes = All.MaxPart * sizeof(int));
+    bytes_tot += bytes;
 
-  NextInTimeBin = (int *) mymalloc("NextInTimeBin", bytes = All.MaxPart * sizeof(int));
-  bytes_tot += bytes;
+    NextInTimeBin = (int *) mymalloc("NextInTimeBin", bytes = All.MaxPart * sizeof(int));
+    bytes_tot += bytes;
 
-  PrevInTimeBin = (int *) mymalloc("PrevInTimeBin", bytes = All.MaxPart * sizeof(int));
-  bytes_tot += bytes;
+    PrevInTimeBin = (int *) mymalloc("PrevInTimeBin", bytes = All.MaxPart * sizeof(int));
+    bytes_tot += bytes;
 
 
-  if(All.MaxPart > 0)
+    if(All.MaxPart > 0)
     {
-      if(!(P = (struct particle_data *) mymalloc("P", bytes = All.MaxPart * sizeof(struct particle_data))))
-	{
-	  printf("failed to allocate memory for `P' (%g MB).\n", bytes / (1024.0 * 1024.0));
-	  endrun(1);
-	}
-      bytes_tot += bytes;
+        if(!(P = (struct particle_data *) mymalloc("P", bytes = All.MaxPart * sizeof(struct particle_data))))
+        {
+            printf("failed to allocate memory for `P' (%g MB).\n", bytes / (1024.0 * 1024.0));
+            endrun(1);
+        }
+        bytes_tot += bytes;
 
-      if(ThisTask == 0)
-	printf("\nAllocated %g MByte for particle storage.\n\n", bytes_tot / (1024.0 * 1024.0));
+        if(ThisTask == 0)
+            printf("\nAllocated %g MByte for particle storage.\n\n", bytes_tot / (1024.0 * 1024.0));
     }
 
-  if(All.MaxPartSph > 0)
+    if(All.MaxPartSph > 0)
     {
-      bytes_tot = 0;
+        bytes_tot = 0;
 
-      if(!
-	 (SphP =
-	  (struct sph_particle_data *) mymalloc("SphP", bytes =
-						All.MaxPartSph * sizeof(struct sph_particle_data))))
-	{
-	  printf("failed to allocate memory for `SphP' (%g MB).\n", bytes / (1024.0 * 1024.0));
-	  endrun(1);
-	}
-      bytes_tot += bytes;
+        if(!
+                (SphP =
+                 (struct sph_particle_data *) mymalloc("SphP", bytes =
+                     All.MaxPartSph * sizeof(struct sph_particle_data))))
+        {
+            printf("failed to allocate memory for `SphP' (%g MB).\n", bytes / (1024.0 * 1024.0));
+            endrun(1);
+        }
+        bytes_tot += bytes;
 
-      if(ThisTask == 0)
-	printf("Allocated %g MByte for storage of SPH data.\n\n", bytes_tot / (1024.0 * 1024.0));
+        if(ThisTask == 0)
+            printf("Allocated %g MByte for storage of SPH data.\n\n", bytes_tot / (1024.0 * 1024.0));
 
     }
 }
