MP-Gadget/MP-Gadget

commit 2ade67fe77747ba6fa7499dbf027e33e8a78a8fe

add Metal cooling with the simple Metallicity.

io.c
@@ -965,7 +965,7 @@ void fill_write_buffer(enum iofields blocknr, int *startindex, int pc, int type)
                     /* get cooling time */
                     u = SPHP(pindex).Entropy / GAMMA_MINUS1 * pow(SPHP(pindex).d.Density * a3inv, GAMMA_MINUS1);
 
-                    tcool = GetCoolingTime(u, SPHP(pindex).d.Density * a3inv, &ne);
+                    tcool = GetCoolingTime(u, SPHP(pindex).d.Density * a3inv, &ne, P[pindex].Metallicity);
 
                     /* convert cooling time with current thermal energy to du/dt */
                     if(tcool != 0)

sfr_eff.c
@@ -68,8 +68,8 @@ void cooling_only(void)		/* normal cooling routine when star formation is disabl
             ne = SPHP(i).Ne;	/* electron abundance (gives ionization state and mean molecular weight) */
             unew = DoCooling(DMAX(All.MinEgySpec,
                         (SPHP(i).Entropy + SPHP(i).e.DtEntropy * dt) /
-                        GAMMA_MINUS1 * pow(SPHP(i).EOMDensity * a3inv, GAMMA_MINUS1))
-                    , SPHP(i).d.Density * a3inv, dtime, &ne);
+                        GAMMA_MINUS1 * pow(SPHP(i).EOMDensity * a3inv, GAMMA_MINUS1)), 
+                        SPHP(i).d.Density * a3inv, dtime, &ne, P[i].Metallicity);
             SPHP(i).Ne = ne;
 
             if(P[i].TimeBin)	/* upon start-up, we need to protect against dt==0 */
@@ -303,7 +303,7 @@ void cooling_and_starformation(void)
                     }
                 }
 #else
-                unew = DoCooling(unew, SPHP(i).d.Density * a3inv, dtime, &ne);
+                unew = DoCooling(unew, SPHP(i).d.Density * a3inv, dtime, &ne, P[i].Metallicity);
 
                 SPHP(i).Ne = ne;
 
@@ -341,7 +341,7 @@ void cooling_and_starformation(void)
 
                 ne = SPHP(i).Ne;
 
-                tcool = GetCoolingTime(egyhot, SPHP(i).d.Density * a3inv, &ne);
+                tcool = GetCoolingTime(egyhot, SPHP(i).d.Density * a3inv, &ne, P[i].Metallicity);
 
                 SPHP(i).Ne = ne;
 
@@ -414,7 +414,7 @@ void cooling_and_starformation(void)
 
                             if(egycurrent > egyeff)
                             {
-                                tcool = GetCoolingTime(egycurrent, SPHP(i).d.Density * a3inv, &ne);
+                                tcool = GetCoolingTime(egycurrent, SPHP(i).d.Density * a3inv, &ne, P[i].Metallicity);
 
                                 if(tcool < trelax && tcool > 0)
                                     trelax = tcool;
@@ -696,8 +696,8 @@ double get_starformation_rate(int i)
 
     ne = SPHP(i).Ne;
 
-    tcool = GetCoolingTime(egyhot, SPHP(i).d.Density * a3inv, &ne);
-    y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
+    tcool = GetCoolingTime(egyhot, SPHP(i).d.Density * a3inv, &ne, P[i].Metallicity);
+    y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold, P[i].Metallicity);
 
     x = 1 + 1 / (2 * y) - sqrt(1 / y + 1 / (4 * y * y));
 
@@ -750,8 +750,14 @@ void init_clouds(void)
         }
 
         ne = 1.0;
+
         SetZeroIonization();
-        tcool = GetCoolingTime(egyhot, dens, &ne);
+
+        /*XXX: We set the threshold without metal cooling;
+         * It probably make sense to set the parameters with
+         * a metalicity dependence.
+         * */
+        tcool = GetCoolingTime(egyhot, dens, &ne, 0.0);
 
         coolrate = egyhot / tcool / dens;
 
@@ -781,7 +787,7 @@ void init_clouds(void)
             egyhot = All.EgySpecSN / (1 + factorEVP) + All.EgySpecCold;
 
             ne = 0.5;
-            tcool = GetCoolingTime(egyhot, dens, &ne);
+            tcool = GetCoolingTime(egyhot, dens, &ne, 0.0);
 
             y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
             x = 1 + 1 / (2 * y) - sqrt(1 / y + 1 / (4 * y * y));
@@ -799,7 +805,7 @@ void init_clouds(void)
             egyhot = All.EgySpecSN / (1 + factorEVP) + All.EgySpecCold;
 
             ne = 0.5;
-            tcool = GetCoolingTime(egyhot, dens, &ne);
+            tcool = GetCoolingTime(egyhot, dens, &ne, 0.0);
 
             y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
             x = 1 + 1 / (2 * y) - sqrt(1 / y + 1 / (4 * y * y));
@@ -887,7 +893,7 @@ void integrate_sfr(void)
         egyhot = All.EgySpecSN / (1 + factorEVP) + All.EgySpecCold;
 
         ne = 1.0;
-        tcool = GetCoolingTime(egyhot, rho, &ne);
+        tcool = GetCoolingTime(egyhot, rho, &ne, 0.0);
 
         y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
         x = 1 + 1 / (2 * y) - sqrt(1 / y + 1 / (4 * y * y));
@@ -931,7 +937,7 @@ void integrate_sfr(void)
                 egyhot = All.EgySpecSN / (1 + factorEVP) + All.EgySpecCold;
 
                 ne = 1.0;
-                tcool = GetCoolingTime(egyhot, rho, &ne);
+                tcool = GetCoolingTime(egyhot, rho, &ne, 0.0);
 
                 y =
                     tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
@@ -945,7 +951,7 @@ void integrate_sfr(void)
                 tsfr2 = sqrt(All.PhysDensThresh / rho2) * All.MaxSfrTimescale;
                 factorEVP2 = pow(rho2 / All.PhysDensThresh, -0.8) * All.FactorEVP;
                 egyhot2 = All.EgySpecSN / (1 + factorEVP) + All.EgySpecCold;
-                tcool2 = GetCoolingTime(egyhot2, rho2, &ne);
+                tcool2 = GetCoolingTime(egyhot2, rho2, &ne, 0.0);
                 y2 =
                     tsfr2 / tcool2 * egyhot2 / (All.FactorSN * All.EgySpecSN -
                             (1 - All.FactorSN) * All.EgySpecCold);

allvars.h
@@ -909,6 +909,7 @@ extern struct global_data_all_processes
     /* some filenames */
     char InitCondFile[100],
          TreeCoolFile[100],
+         MetalCoolFile[100],
          OutputDir[100],
          SnapshotFileBase[100],
          EnergyFile[100],

begrun.c
@@ -1050,6 +1050,10 @@ void read_parameter_file(char *fname)
         addr[nt] = All.TreeCoolFile;
         id[nt++] = STRING;
 
+        strcpy(tag[nt], "MetalCoolFile");
+        addr[nt] = All.MetalCoolFile;
+        id[nt++] = STRING;
+
         strcpy(tag[nt], "DensityKernelType");
         addr[nt] = &All.DensityKernelType;
         id[nt++] = INT;

cooling.c
@@ -11,6 +11,7 @@
 #ifdef COOLING
 /* used at init for caculating the star formation threshold*/
 static int ZeroIonizationFlag = 0;
+static int CoolingNoMetal = 1;
 /* hydrogen abundance by mass */
 
 #define XH HYDROGEN_MASSFRAC
@@ -80,35 +81,53 @@ struct {
     double * nHp_table;
     double * nHep_table;
     double * nHepp_table;
-    double * Lpnet_table; /* primordial cooling - heating + CMB Compton*/
+    double * Lpnet_table; /* primordial cooling - UVB heating + CMB Compton*/
     double * T_table;
 
     Interp interp;
     Interp interpT;
 } PC;
 
+struct {
+    int NRedshift_bins;
+    double * Redshift_bins;
+
+    int NHydrogenNumberDensity_bins;
+    double * HydrogenNumberDensity_bins;
+
+    int NTemperature_bins;
+    double * Temperature_bins;
+
+    double * Lmet_table; /* metal cooling @ one solar metalicity*/
+
+    Interp interp;
+} MC;
+
 void InitCool(void) {
+    /* XXX: we assume uniform bins. otherwise interp will fail! */
+
+    int size;  /* this variable is not checked. if the file was inconsistent we ruin the sim! */
 
     PC.Redshift_bins = h5readdouble(All.TreeCoolFile, "Redshift_bins", &PC.NRedshift_bins);
     PC.HydrogenNumberDensity_bins = h5readdouble(All.TreeCoolFile, "HydrogenNumberDensity_bins", &PC.NHydrogenNumberDensity_bins);
     PC.Temperature_bins = h5readdouble(All.TreeCoolFile, "Temperature_bins", &PC.NTemperature_bins);
     PC.SpecInternalEnergy_bins = h5readdouble(All.TreeCoolFile, "SpecInternalEnergy_bins", &PC.NSpecInternalEnergy_bins);
-    int size;
+
     PC.nHp_table = h5readdouble(All.TreeCoolFile, "nHp", &size);
     PC.nHep_table = h5readdouble(All.TreeCoolFile, "nHep", &size);
     PC.nHepp_table = h5readdouble(All.TreeCoolFile, "nHepp", &size);
     PC.Lpnet_table = h5readdouble(All.TreeCoolFile, "NetCoolingRate", &size);
     PC.T_table = h5readdouble(All.TreeCoolFile, "EquilibriumTemperature", &size);
 
-    int dims[] = {PC.NRedshift_bins, PC.NHydrogenNumberDensity_bins, PC.NTemperature_bins};
 
-    /* we assume uniform bins. otherwise interp will fail! */
-    interp_init(&PC.interp, 3, dims);
-    interp_init_dim(&PC.interp, 0, PC.Redshift_bins[0], PC.Redshift_bins[PC.NRedshift_bins - 1]);
-    interp_init_dim(&PC.interp, 1, PC.HydrogenNumberDensity_bins[0], 
-                    PC.HydrogenNumberDensity_bins[PC.NHydrogenNumberDensity_bins - 1]);
-    interp_init_dim(&PC.interp, 2, PC.Temperature_bins[0], 
-                    PC.Temperature_bins[PC.NTemperature_bins - 1]);
+    /* 
+     *
+     * initialize the temperature / spec internal energy look up table,
+     * This table is based on Primordial cooling and UVB. Metal is ignored, as
+     * argued in Vogelsberger et al  2013 Arepo paper.
+     *
+     * */
+    {
 
     int dimsT[] = {PC.NRedshift_bins, PC.NHydrogenNumberDensity_bins, PC.NSpecInternalEnergy_bins};
     interp_init(&PC.interpT, 3, dimsT);
@@ -117,9 +136,60 @@ void InitCool(void) {
                     PC.HydrogenNumberDensity_bins[PC.NHydrogenNumberDensity_bins - 1]);
     interp_init_dim(&PC.interpT, 2, PC.SpecInternalEnergy_bins[0], 
                     PC.SpecInternalEnergy_bins[PC.NSpecInternalEnergy_bins - 1]);
+
+    }
     printf("z = %g log nH = %g log U = %g logT = %g\n",
             157., -0.1, 10.0,
             TableTemperature(157, 10.0, -0.1));
+
+
+    /* initialize the primordial cooling table (H and He + UVB) */
+    {
+        int dims[] = {PC.NRedshift_bins, PC.NHydrogenNumberDensity_bins, PC.NTemperature_bins};
+
+        interp_init(&PC.interp, 3, dims);
+        interp_init_dim(&PC.interp, 0, PC.Redshift_bins[0], PC.Redshift_bins[PC.NRedshift_bins - 1]);
+        interp_init_dim(&PC.interp, 1, PC.HydrogenNumberDensity_bins[0], 
+                        PC.HydrogenNumberDensity_bins[PC.NHydrogenNumberDensity_bins - 1]);
+        interp_init_dim(&PC.interp, 2, PC.Temperature_bins[0], 
+                        PC.Temperature_bins[PC.NTemperature_bins - 1]);
+    }
+
+    /* now initialize the metal cooling table from cloudy; we got this file
+     * from vogelsberger's Arepo simulations; it is supposed to be 
+     * cloudy + UVB - H and He; look so.
+     * the table contains only 1 Z_sun values. Need to be scaled to the 
+     * metallicity.
+     *
+     * */
+            /* let's see if the Metal Cool File is magic NoMetal */ 
+    if(!strcmp(All.MetalCoolFile, "NoMetal")) {
+        CoolingNoMetal = 0;
+    } else {
+        CoolingNoMetal = 1;
+        MC.Redshift_bins = h5readdouble(All.MetalCoolFile, "Redshift_bins", &MC.NRedshift_bins);
+        MC.HydrogenNumberDensity_bins = h5readdouble(All.MetalCoolFile, "HydrogenNumberDensity_bins", &MC.NHydrogenNumberDensity_bins);
+        MC.Temperature_bins = h5readdouble(All.MetalCoolFile, "Temperature_bins", &MC.NTemperature_bins);
+        MC.Lmet_table = h5readdouble(All.MetalCoolFile, "NetCoolingRate", &size);
+
+        double * tabbedmet = h5readdouble(All.MetalCoolFile, "Metallicity_bins", &size);
+
+        if(ThisTask == 0 && size != 1 || tabbedmet[0] != 0.0) {
+            fprintf(stderr, "MetalCool file %s is wrongly tabulated\n", All.MetalCoolFile);
+            endrun(124214);
+        }
+        free(tabbedmet);
+
+        int dims[] = {MC.NRedshift_bins, MC.NHydrogenNumberDensity_bins, MC.NTemperature_bins};
+
+        interp_init(&PC.interp, 3, dims);
+        interp_init_dim(&MC.interp, 0, MC.Redshift_bins[0], MC.Redshift_bins[MC.NRedshift_bins - 1]);
+        interp_init_dim(&MC.interp, 1, MC.HydrogenNumberDensity_bins[0], 
+                        MC.HydrogenNumberDensity_bins[MC.NHydrogenNumberDensity_bins - 1]);
+        interp_init_dim(&MC.interp, 2, MC.Temperature_bins[0], 
+                        MC.Temperature_bins[MC.NTemperature_bins - 1]);
+    
+    }
 }
 
 static void TableAbundance(double redshift, double logT, double lognH, struct abundance * y) {
@@ -143,7 +213,7 @@ static void TableAbundance(double redshift, double logT, double lognH, struct ab
     y->ne = y->nHp + y->nHep + 2.0 * y->nHepp;
 }
 
-/* the table stores net cooling rate. */
+/* the table stores net cooling rate of primoridal H + He + UVB heating. */
 static double TableCoolingRate(double redshift, double logT, double lognH) {
     if(ZeroIonizationFlag) redshift = -1;
 
@@ -161,6 +231,17 @@ static double TableCoolingRate(double redshift, double logT, double lognH) {
         double LambdaCmptn = 5.65e-36 * ne * (T - 2.73 * (1. + redshift)) * pow(1. + redshift, 4.) / nH;
         rate = LambdaFF + LambdaCmptn;
     }
+    return rate;
+}
+
+
+/* the table stores net cooling rate of Metals emerged in the same UVB at 1 * Z_sun. */
+static double TableMetalCoolingRate(double redshift, double logT, double lognH) {
+    double x[] = {redshift, lognH, logT};
+    int status[3];
+    double rate = interp_eval(&MC.interp, x, MC.Lmet_table, status);
+    /* XXX: in case of very hot / very dense we just use whatever the table says at
+     * the limit. should be OK. Ask Tiziana about this */
     return rate;
 }
 
@@ -202,10 +283,15 @@ static double TableTemperature(double redshift, double logU, double lognH) {
  * This is different from old GADGET. 
  *
  * */
-static double HeatingRateU(double redshift, double u, double lognH) {
+static double HeatingRateU(double redshift, double u, double lognH, double Z) {
 
     double logT = TableTemperature(redshift, log10(u), lognH);
-    return - TableCoolingRate(redshift, logT, lognH);
+    double rate = - TableCoolingRate(redshift, logT, lognH);
+
+    if (Z != 0 && !CoolingNoMetal ) {
+        rate -= Z * TableCoolingRate(redshift, logT, lognH);
+    }
+    return rate;
 }
 
 /* returns abundance ratios
@@ -245,7 +331,7 @@ double AbundanceRatios(double u, double rho, double *ne, double *nH0, double *nH
  * the electron abundance is calculated too.
  *
  */
-double DoCooling(double u_old, double rho, double dt, double *ne_guess)
+double DoCooling(double u_old, double rho, double dt, double *ne_guess, double Z)
 {
     double u, du;
     double u_lower, u_upper;
@@ -281,7 +367,7 @@ double DoCooling(double u_old, double rho, double dt, double *ne_guess)
     u_upper = u;
 
     /**/
-    LambdaNet = HeatingRateU(redshift, u, lognH);
+    LambdaNet = HeatingRateU(redshift, u, lognH, Z);
 
     /* bracketing */
 
@@ -289,7 +375,7 @@ double DoCooling(double u_old, double rho, double dt, double *ne_guess)
     {
         u_upper *= sqrt(1.1);
         u_lower /= sqrt(1.1);
-            while(u_upper - u_old - ratefact * HeatingRateU(redshift, u_upper, lognH) * dt < 0)
+            while(u_upper - u_old - ratefact * HeatingRateU(redshift, u_upper, lognH, Z) * dt < 0)
             {
                 u_upper *= 1.1;
                 u_lower *= 1.1;
@@ -301,7 +387,7 @@ double DoCooling(double u_old, double rho, double dt, double *ne_guess)
     {
         u_lower /= sqrt(1.1);
         u_upper *= sqrt(1.1);
-            while(u_lower - u_old - ratefact * HeatingRateU(redshift, u_lower, lognH) * dt > 0)
+            while(u_lower - u_old - ratefact * HeatingRateU(redshift, u_lower, lognH, Z) * dt > 0)
             {
                 u_upper /= 1.1;
                 u_lower /= 1.1;
@@ -312,7 +398,7 @@ double DoCooling(double u_old, double rho, double dt, double *ne_guess)
     {
         u = 0.5 * (u_lower + u_upper);
 
-        LambdaNet = HeatingRateU(redshift, u, lognH);
+        LambdaNet = HeatingRateU(redshift, u, lognH, Z);
 
         if(u - u_old - ratefact * LambdaNet * dt > 0)
         {
@@ -351,7 +437,7 @@ double DoCooling(double u_old, double rho, double dt, double *ne_guess)
 /* returns cooling time. 
  * NOTE: If we actually have heating, a cooling time of 0 is returned.
  */
-double GetCoolingTime(double u_old, double rho, double *ne_guess)
+double GetCoolingTime(double u_old, double rho, double *ne_guess, double Z)
 {
     double u;
     double LambdaNet, coolingtime;
@@ -376,7 +462,7 @@ double GetCoolingTime(double u_old, double rho, double *ne_guess)
 
     u = u_old;
 
-    LambdaNet = HeatingRateU(redshift, u, lognH);
+    LambdaNet = HeatingRateU(redshift, u, lognH, Z);
 
     /* bracketing */
 

cooling.h
@@ -1,39 +1,11 @@
-#ifndef INLINE_FUNC
-#ifdef INLINE
-#define INLINE_FUNC inline
-#else
-#define INLINE_FUNC
-#endif
-#endif
-
-
 double AbundanceRatios(double u, double rho, double *ne_guess, double *nH0_pointer, double *nHeII_pointer);
-double convert_u_to_temp(double u, double rho, double *ne_guess);
-#ifdef LT_METAL_COOLING
 double GetCoolingTime(double u_old, double rho,  double *ne_guess, double Z);
-double CoolingRateFromU(double u, double rho, double *ne_guess, double Z);
 double DoCooling(double u_old, double rho, double dt, double *ne_guess, double Z);
-double CoolingRate(double logT, double rho, double *nelec, double Z);
-double DoInstabilityCooling(double m_old, double u, double rho, double dt, double fac, double *ne_guess, double Z);
-#else
-double CoolingRate(double logT, double rho, double *nelec);
-double CoolingRateFromU(double u, double rho, double *ne_guess);
-double DoCooling(double u_old, double rho, double dt, double *ne_guess);
-double GetCoolingTime(double u_old, double rho,  double *ne_guess);
-double DoInstabilityCooling(double m_old, double u, double rho, double dt, double fac, double *ne_guess);
-#endif
 
-void   find_abundances_and_rates(double logT, double rho, double *ne_guess);
 void   InitCool(void);
-void   InitCoolMemory(void);
 void   IonizeParams(void);
-void   IonizeParamsFunction(void);
-void   IonizeParamsTable(void);
-double INLINE_FUNC LogTemp(double u, double ne);
 void   MakeCoolingTable(void);
-void   ReadIonizeParams(char *fname);
 void   SetZeroIonization(void);
-void   TestCool(void);
 
 
 

commit 309742a132f62c9cc2768f9a1d4662747be41665

fix a few serious bugs in SFR.
a typo wrongly takes metallicity in calculation -- this causes a blow in bh
accretion rate.

also mimic the old code's behavior of reseting Ne to egyhot Ne.
This bug is introduced since 2ade67fe77747ba6fa7499dbf027e33e8a78a8fe .

io.c
@@ -574,7 +574,7 @@ void fill_write_buffer(enum iofields blocknr, int *startindex, int pc, int type)
                 if(P[pindex].Type == type)
                 {
                 /* convert to solar masses per yr */
-                    *fp++ = get_starformation_rate(pindex, NULL, NULL) 
+                    *fp++ = get_starformation_rate(pindex) 
                         * ((All.UnitMass_in_g / SOLAR_MASS) / (All.UnitTime_in_s / SEC_PER_YEAR));
                     n++;
                 }

proto.h
@@ -349,7 +349,7 @@ int get_timestep(int p, double *a, int flag);
 
 void determine_PMinterior(void);
 
-double get_starformation_rate(int i, double * trelax, double * egyeff);
+double get_starformation_rate(int i);
 void gravity_tree(void);
 void hydro_force(void);
 void init(void);

sfr_eff.c
@@ -27,6 +27,7 @@ static int make_particle_wind(int i);
 static int make_particle_star(int i, int number_of_stars_generated);
 static double get_sfr_factor_due_to_selfgravity(int i);
 static double get_sfr_factor_due_to_h2(int i);
+static double get_starformation_rate_full(int i, double dtime, double * ne_new, double * trelax, double * egyeff);
 
 
 #ifdef WINDS_VS08
@@ -698,15 +699,8 @@ static void starformation(int i) {
 
     double dtime = dt / All.cf.hubble;
 
-    /* 
-     * gadget-p doesn't have this cap.
-     * without the cap sm can be bigger than cloudmass.
-        if(tsfr < dtime)
-            tsfr = dtime;
-    */
-
     double egyeff, trelax;
-    double rateOfSF = get_starformation_rate(i, &trelax, &egyeff);
+    double rateOfSF = get_starformation_rate_full(i, dtime, &SPHP(i).Ne, &trelax, &egyeff);
 
     /* amount of stars expect to form */
 
@@ -764,8 +758,12 @@ static void starformation(int i) {
 
 }
 
-double get_starformation_rate(int i, double * trelax, double * egyeff)
-{
+double get_starformation_rate(int i) {
+    /* returns SFR in internal units */
+    return get_starformation_rate_full(i, 1e9, NULL, NULL, NULL);
+}
+
+static double get_starformation_rate_full(int i, double dtime, double * ne_new, double * trelax, double * egyeff) {
     double rateOfSF;
     int flag;
     double tsfr;
@@ -790,6 +788,13 @@ double get_starformation_rate(int i, double * trelax, double * egyeff)
     }
 
     tsfr = sqrt(All.PhysDensThresh / (SPHP(i).d.Density * All.cf.a3inv)) * All.MaxSfrTimescale;
+    /* 
+     * gadget-p doesn't have this cap.
+     * without the cap sm can be bigger than cloudmass.
+    */
+    if(tsfr < dtime)
+        tsfr = dtime;
+
 
     factorEVP = pow(SPHP(i).d.Density * All.cf.a3inv / All.PhysDensThresh, -0.8) * All.FactorEVP;
 
@@ -798,13 +803,17 @@ double get_starformation_rate(int i, double * trelax, double * egyeff)
     ne = SPHP(i).Ne;
 
     tcool = GetCoolingTime(egyhot, SPHP(i).d.Density * All.cf.a3inv, &ne, P[i].Metallicity);
-    y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold, P[i].Metallicity);
+    y = tsfr / tcool * egyhot / (All.FactorSN * All.EgySpecSN - (1 - All.FactorSN) * All.EgySpecCold);
 
     x = 1 + 1 / (2 * y) - sqrt(1 / y + 1 / (4 * y * y));
 
     cloudmass = x * P[i].Mass;
 
     rateOfSF = (1 - All.FactorSN) * cloudmass / tsfr;
+
+    if (ne_new ) {
+        *ne_new = ne;
+    }
 
     if (trelax) {
         *trelax = tsfr * (1 - x) / x / (All.FactorSN * (1 + factorEVP));