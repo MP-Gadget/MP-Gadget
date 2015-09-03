30341725352cfbd972056f635ce5752023676cec
Yu Feng <yfeng1@h2ologin3.ncsa.illinois.edu>
2015-09-03 12:40:48 -0500

    skip writing sfr details.

--- sfr_eff.c
+++ sfr_eff.c
@@ -694,12 +694,12 @@ static int make_particle_wind(int i, double v, double vmean[3]) {
     {
         for(j = 0; j < 3; j++)
             dir[j] /= norm;
-
+#if 0
         fprintf(FdSfrDetails, 
              "Wind T %g %lu M %g P %g %g %g V %g VC %g %g %g D %g %g %g\n",
                 All.Time, P[i].ID, P[i].Mass, P[i].Pos[0], P[i].Pos[1], P[i].Pos[2], 
                 v / All.cf.a, vmean[0], vmean[1], vmean[2], dir[0], dir[1], dir[2]);
-
+#endif
         for(j = 0; j < 3; j++)
         {
             P[i].Vel[j] += v * dir[j];
@@ -711,9 +711,11 @@ static int make_particle_wind(int i, double v, double vmean[3]) {
 }
 
 static int make_particle_star(int i, int number_of_stars_generated) {
+#if 0
     fprintf(FdSfrDetails, "Star T %g %lu M %g RHOP %g P %g %g %g\n",
             All.Time, P[i].ID, P[i].Mass, SPHP(i).d.Density * All.cf.a3inv, 
             P[i].Pos[0], P[i].Pos[1], P[i].Pos[2]);
+#endif
     /* ok, make a star */
     if(number_of_stars_generated == (GENERATIONS - 1))
     {
