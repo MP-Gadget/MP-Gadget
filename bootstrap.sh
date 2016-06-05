6fb44655d9936c2e3722087df7108861a28248ea
Yu Feng <rainwoodman@gmail.com>
2016-06-05 15:57:44 -0700

    Add bootstrap script.
    (to start clean from git clone)

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,17 @@
 
 git submodule init
 git submodule update
+
+cat <<EOF
+If there were no errors fetching dependency submodules
+then it is time to build MP-Gadget.
+
+copy Options.mk.example to Opitions.mk,
+
+and modify the file 
+
+then type
+
+make
+
+EOF
