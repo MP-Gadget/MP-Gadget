ba739ea9bfcf5ed689aa84e43af59233c0b1b2ca
Yu Feng <rainwoodman@gmail.com>
2016-07-22 18:00:46 -0700

    Missing utils functions.

---
+++ utils-string.h
@@ -0,0 +1,24 @@
+#ifndef _FASTPM_STRING_H_
+#define _FASTPM_STRING_H_
+
+#include <stdarg.h>
+
+char *
+fastpm_file_get_content(const char * filename);
+
+char **
+fastpm_strsplit(const char * str, const char * split);
+
+char *
+fastpm_strdup(const char * str);
+
+char *
+fastpm_strdup_printf(const char * fmt, ...);
+
+char *
+fastpm_strdup_vprintf(const char * fmt, va_list va);
+
+void
+fastpm_path_ensure_dirname(const char * path);
+
+#endif
