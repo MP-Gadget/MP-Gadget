d133523137ab68c286f8b44e8a5bf38b1fc82ea7
Yu Feng <rainwoodman@gmail.com>
2016-08-02 13:56:24 -0700

    PFFT_TUNE is rumored to make FFT a lot faster..
    (e.g. in the manual). Will this speed up PM?

--- petapm.c
+++ petapm.c
@@ -192,10 +192,10 @@ void petapm_init(double BoxSize, int _Nmesh, int Nthreads) {
 
     plan_forw = pfft_plan_dft_r2c_3d(
         n, real, rho_k, comm_cart_2d, PFFT_FORWARD, 
-        PFFT_TRANSPOSED_OUT | PFFT_ESTIMATE | PFFT_DESTROY_INPUT);    
+        PFFT_TRANSPOSED_OUT | PFFT_ESTIMATE | PFFT_TUNE | PFFT_DESTROY_INPUT);
     plan_back = pfft_plan_dft_c2r_3d(
         n, complx, real, comm_cart_2d, PFFT_BACKWARD, 
-        PFFT_TRANSPOSED_IN | PFFT_ESTIMATE | PFFT_DESTROY_INPUT);    
+        PFFT_TRANSPOSED_IN | PFFT_ESTIMATE | PFFT_TUNE | PFFT_DESTROY_INPUT);
 
     pm_free();
 
