dc7734be76d22ed66ddeea2c1974fa28d2f1f777
Yu Feng <rainwoodman@gmail.com>
2016-08-28 10:08:07 -0700

    remove breakpoint and print timestamps.

--- endrun.c
+++ endrun.c
@@ -6,6 +6,7 @@
 #include "allvars.h"
 #include "endrun.h"
 
+static double _timestart = -1;
 /*  This function aborts the simulation.
  *
  *  if ierr > 0, the error is uncollective.
@@ -14,22 +15,22 @@
 
 void endrun(int ierr, const char * fmt, ...)
 {
+    if(_timestart < 0) _timestart = MPI_Wtime();
+
     va_list va;
     char buf[4096];
     va_start(va, fmt);
     vsprintf(buf, fmt, va);
     va_end(va);
     if(ierr > 0) {
-        printf("Task %d: Error (%d) %s\n", ThisTask, ierr, buf);
+        printf("[ %09.2f ] Task %d: %s", MPI_Wtime() - _timestart, ThisTask, buf);
         fflush(stdout);
-        BREAKPOINT;
         MPI_Abort(MPI_COMM_WORLD, ierr);
     } else {
         if(ThisTask == 0) {
-            printf("Collective Error (%d) %s\n", ierr, buf);
+            printf("[ %09.2f ] %s", MPI_Wtime() - _timestart, buf);
             fflush(stdout);
         }
-        BREAKPOINT;
         MPI_Abort(MPI_COMM_WORLD, ierr);
     }
 }
@@ -42,8 +43,7 @@ void endrun(int ierr, const char * fmt, ...)
 
 void message(int ierr, const char * fmt, ...)
 {
-    static double start = -1;
-    if(start < 0) start = MPI_Wtime();
+    if(_timestart < 0) _timestart = MPI_Wtime();
 
     va_list va;
     char buf[4096];
@@ -52,12 +52,12 @@ void message(int ierr, const char * fmt, ...)
     va_end(va);
     /* FIXME: deal with \n in the buf. */
     if(ierr > 0) {
-        printf("[ %09.2f ] Task %d: %s", MPI_Wtime() - start, ThisTask, buf);
+        printf("[ %09.2f ] Task %d: %s", MPI_Wtime() - _timestart, ThisTask, buf);
         fflush(stdout);
     } else {
         MPI_Barrier(MPI_COMM_WORLD);
         if(ThisTask == 0) {
-            printf("[ %09.2f ] %s", MPI_Wtime() - start, buf);
+            printf("[ %09.2f ] %s", MPI_Wtime() - _timestart, buf);
             fflush(stdout);
         }
     }
