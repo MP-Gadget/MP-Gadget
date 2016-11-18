9e62dc8603b9e122ee12d8c90d4c693c6b6d8a61
Yu Feng <rainwoodman@gmail.com>
2016-11-18 14:12:55 -0800

    remove unused run dir.

--- run/run.sh
+++
@@ -1,3 +0,0 @@
-export OMP_NUM_THREADS=1
-mpirun -np 4 ../build/MP-GenIC paramfile.genic || exit 1
-mpirun -np 4 ../build/MP-Gadget paramfile.gadget || exit 1
--- run/runfof.sh
+++
@@ -1,5 +0,0 @@
-export OMP_NUM_THREADS=1
-mpirun -np 4 ../build/MP-Gadget paramfile.fof 3 003
-
-cd fofdebug
-python assert.py
