fa737731c48bb5d923cc71f5fd54aabbfc8e90a2
Simeon Bird <sbird4@jhu.edu>
2017-07-12 10:24:44 -0400

    Localise MaxTiStepDisplacement to timestep.c

--- allvars.h
+++ allvars.h
@@ -351,7 +351,6 @@ extern struct global_data_all_processes
 
     int Ti_Current;		/*!< current time on integer timeline */
     int Ti_nextoutput;		/*!< next output time on integer timeline */
-    int MaxTiStepDisplacement; /*!< Maximum (PM) integer timestep from global displacements*/
     int PM_Ti_endstep, PM_Ti_begstep;
     int PM_Ti_kick;            /* current time stamp of the PM component of the momentum */
     int Nmesh;
