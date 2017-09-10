b20371a827113ca91a2ed9d8d7427befdd0a6bef
Simeon Bird <sbird4@jhu.edu>
2017-09-10 16:51:36 -0400

    Find output using current time, not last output time, which may be slightly earlier

--- run.c
+++ run.c
@@ -178,7 +178,7 @@ void run(void)
 
             /*Do the extra half-kick we avoided for a snapshot.*/
             /*Find next output*/
-            Ti_nextoutput = find_next_outputtime(Ti_nextoutput);
+            Ti_nextoutput = find_next_outputtime(All.Ti_Current);
             if(out_from_ti(Ti_nextoutput) < All.OutputListLength)
                 message(0, "Setting next time for snapshot file to Time_next= %g \n",
                         exp(All.OutputListTimes[out_from_ti(Ti_nextoutput)]));
