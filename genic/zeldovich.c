71c968cc81746f54d13d6b2dbefc02160e992eaf
Yu Feng <rainwoodman@gmail.com>
2017-09-15 13:34:09 -0700

    FastPM's gaussian filler; more consistent with ngenic and nbodykit

--- genic/zeldovich.c
+++ genic/zeldovich.c
@@ -178,126 +178,6 @@ void displacement_fields() {
  * force from potential
  *
  *********************/
-static void gaussian_fill(PetaPMRegion * region, pfft_complex * rho_k) {
-    gsl_rng * random_generator_seed = gsl_rng_alloc(gsl_rng_ranlxd1);
-    gsl_rng_set(random_generator_seed, Seed);
-
-    unsigned int * seedtable = mymalloc("Seedtable", sizeof(unsigned int) * Nmesh * Nmesh);
-
-#define SETSEED(i, j) { \
-    unsigned int seed = 0x7fffffff * gsl_rng_uniform(random_generator_seed); \
-    seedtable[(i) * Nmesh + (j)] = seed; \
-}
-
-    int i;
-    for(i = 0; i < Nmesh / 2; i++)
-    {
-        int j;
-        for(j = 0; j < i; j++) SETSEED(i, j)
-        for(j = 0; j < i + 1; j++) SETSEED(j, i)
-        for(j = 0; j < i; j++) SETSEED(Nmesh - 1 - i, j)
-        for(j = 0; j < i + 1; j++) SETSEED(Nmesh - 1 - j, i)
-        for(j = 0; j < i; j++) SETSEED(i, Nmesh - 1 - j)
-        for(j = 0; j < i + 1; j++) SETSEED(j, Nmesh - 1 - i)
-        for(j = 0; j < i; j++) SETSEED(Nmesh - 1 - i, Nmesh - 1 - j)
-        for(j = 0; j < i + 1; j++) SETSEED(Nmesh - 1 - j, Nmesh - 1 - i)
-    }
-    gsl_rng_free(random_generator_seed);
-
-    for(i = region->offset[2]; i < region->offset[2] + region->size[2]; i ++) {
-        gsl_rng * random_generator0 = gsl_rng_alloc(gsl_rng_ranlxd1);
-        gsl_rng * random_generator1 = gsl_rng_alloc(gsl_rng_ranlxd1);
-        int j;
-        for(j = region->offset[0]; j < region->offset[0] + region->size[0]; j ++) {
-            /* always pull the gaussian from the lower quadrant plane for k = 0
-             * plane*/
-            int hermitian = 0;
-            if(i == 0) {
-                int jj = MESH2K(j);
-                if(jj != j) {
-                    jj = - jj;
-                    hermitian = 1; 
-                }
-                gsl_rng_set(random_generator0, seedtable[i * Nmesh + jj]);
-            } else {
-                int ii = MESH2K(i);
-                if(i != ii) {
-                    ii = - ii;
-                    int jj = j!= 0?(Nmesh - j):0;
-                    hermitian = 1;
-                    gsl_rng_set(random_generator0, seedtable[ii * Nmesh + jj]);
-                }  else {
-                    gsl_rng_set(random_generator0, seedtable[i * Nmesh + j]);
-                }
-            } 
-
-            gsl_rng_set(random_generator1, seedtable[i * Nmesh + j]);
-
-            /* two skips to maintains consistency of generators in lower quandrant */
-            gsl_rng_uniform(random_generator1);
-            gsl_rng_uniform(random_generator1);
-            int k;
-            for(k = 0; k <= Nmesh / 2; k ++) {
-                /* on k = 0 plane, we use the lower quadrant generator, 
-                 * then hermit transform the result if it is nessessary */
-                gsl_rng * random_generator = k?random_generator1:random_generator0;
-                hermitian *= k==0;
-
-                ptrdiff_t ip = region->strides[0] * (j - region->offset[0]) 
-                             + region->strides[1] * (k - region->offset[1]) 
-                             + region->strides[2] * (i - region->offset[2]);
-                double phase = gsl_rng_uniform(random_generator) * 2 * M_PI;
-                double ampl = 0;
-                do ampl = gsl_rng_uniform(random_generator); while(ampl == 0);
-
-                if(k < region->offset[1]) continue;
-                if(k >= region->offset[1] + region->size[1]) continue;
-
-                double delta = sqrt(- log(ampl));
-                if(i == Nmesh / 2) {
-                    delta = 0;
-                }
-                if(j == Nmesh / 2) {
-                    delta = 0;
-                }
-                if(k >= Nmesh / 2) {
-                    /* this is to cut off at the Nyquist*/
-                    delta = 0;
-                }
-                rho_k[ip][0] = delta * cos(phase);
-                rho_k[ip][1] = delta * sin(phase);
-                if(hermitian) {
-                    rho_k[ip][1] *= -1;
-                }
-            }
-        }
-        gsl_rng_free(random_generator0);
-        gsl_rng_free(random_generator1);
-    }
-    myfree(seedtable);
-#if 0
-    /* dump the gaussian field for debugging 
-     * 
-     * the z directioin is in axis 1.
-     *
-     * */
-    FILE * rhokf = fopen("rhok", "w");
-    printf("strides %td %td %td\n", 
-            region->strides[0],
-            region->strides[1],
-            region->strides[2]);
-    printf("size %td %td %td\n", 
-            region->size[0],
-            region->size[1],
-            region->size[2]);
-    printf("offset %td %td %td\n", 
-            region->offset[0],
-            region->offset[1],
-            region->offset[2]);
-    fwrite(rho_k, sizeof(double) * fftsize, 1, rhokf);
-    fclose(rhokf);
-#endif
-}
 
 static void density_transfer(int64_t k2, int kpos[3], pfft_complex * value) {
     if(k2) {
@@ -371,3 +251,242 @@ static void readout_force_y(int i, double * mesh, double weight) {
 static void readout_force_z(int i, double * mesh, double weight) {
     P[i].Vel[2] += weight * mesh[0];
 }
+
+/* 
+ * The following functions are from fastpm/libfastpm/initialcondition.c.
+ * Agrees with nbodykit's pmesh/whitenoise.c, which agrees with n-genic.
+ * */
+typedef struct {
+    struct {
+        ptrdiff_t start[3];
+        ptrdiff_t size[3];
+        ptrdiff_t strides[3];
+        ptrdiff_t total;
+    } ORegion;
+    int Nmesh[3];
+    size_t allocsize;
+} PM;
+
+static inline void 
+SETSEED(PM * pm, unsigned int * table[2][2], int i, int j, gsl_rng * rng) 
+{ 
+    unsigned int seed = 0x7fffffff * gsl_rng_uniform(rng); 
+
+    int ii[2] = {i, (pm->Nmesh[0] - i) % pm->Nmesh[0]};
+    int jj[2] = {j, (pm->Nmesh[1] - j) % pm->Nmesh[1]};
+    int d1, d2;
+    for(d1 = 0; d1 < 2; d1++) {
+        ii[d1] -= pm->ORegion.start[0];
+        jj[d1] -= pm->ORegion.start[1];
+    }
+    for(d1 = 0; d1 < 2; d1++)
+    for(d2 = 0; d2 < 2; d2++) {
+        if( ii[d1] >= 0 && 
+            ii[d1] < pm->ORegion.size[0] &&
+            jj[d2] >= 0 &&
+            jj[d2] < pm->ORegion.size[1]
+        ) {
+            table[d1][d2][ii[d1] * pm->ORegion.size[1] + jj[d2]] = seed;
+        }
+    }
+}
+static inline unsigned int 
+GETSEED(PM * pm, unsigned int * table[2][2], int i, int j, int d1, int d2) 
+{
+    i -= pm->ORegion.start[0];
+    j -= pm->ORegion.start[1];
+    if(i < 0) abort();
+    if(j < 0) abort();
+    if(i >= pm->ORegion.size[0]) abort();
+    if(j >= pm->ORegion.size[1]) abort();
+    return table[d1][d2][i * pm->ORegion.size[1] + j];
+}
+
+static void 
+SAMPLE(gsl_rng * rng, double * ampl, double * phase)
+{
+    *phase = gsl_rng_uniform(rng) * 2 * M_PI;
+    *ampl = 0;
+    do *ampl = gsl_rng_uniform(rng); while(*ampl == 0);
+}
+
+static void
+pmic_fill_gaussian_gadget(PM * pm, double * delta_k, int seed)
+{
+    /* Fill delta_k with gadget scheme */
+    int d;
+    int i, j, k;
+
+    memset(delta_k, 0, sizeof(delta_k[0]) * pm->allocsize);
+
+    gsl_rng * rng = gsl_rng_alloc(gsl_rng_ranlxd1);
+    gsl_rng_set(rng, seed);
+
+    unsigned int * seedtable[2][2];
+    for(i = 0; i < 2; i ++)
+    for(j = 0; j < 2; j ++) {
+            seedtable[i][j] = calloc(pm->ORegion.size[0] * pm->ORegion.size[1], sizeof(int));
+    }
+
+    for(i = 0; i < pm->Nmesh[0] / 2; i++) {
+        for(j = 0; j < i; j++) SETSEED(pm, seedtable, i, j, rng);
+        for(j = 0; j < i + 1; j++) SETSEED(pm, seedtable, j, i, rng);
+        for(j = 0; j < i; j++) SETSEED(pm, seedtable, pm->Nmesh[0] - 1 - i, j, rng);
+        for(j = 0; j < i + 1; j++) SETSEED(pm, seedtable, pm->Nmesh[1] - 1 - j, i, rng);
+        for(j = 0; j < i; j++) SETSEED(pm, seedtable, i, pm->Nmesh[1] - 1 - j, rng);
+        for(j = 0; j < i + 1; j++) SETSEED(pm, seedtable, j, pm->Nmesh[0] - 1 - i, rng);
+        for(j = 0; j < i; j++) SETSEED(pm, seedtable, pm->Nmesh[0] - 1 - i, pm->Nmesh[1] - 1 - j, rng);
+        for(j = 0; j < i + 1; j++) SETSEED(pm, seedtable, pm->Nmesh[1] - 1 - j, pm->Nmesh[0] - 1 - i, rng);
+    }
+    gsl_rng_free(rng);
+
+    ptrdiff_t irel[3];
+    for(i = pm->ORegion.start[0]; 
+        i < pm->ORegion.start[0] + pm->ORegion.size[0]; 
+        i ++) {
+
+        gsl_rng * lower_rng = gsl_rng_alloc(gsl_rng_ranlxd1);
+        gsl_rng * this_rng = gsl_rng_alloc(gsl_rng_ranlxd1);
+
+        int ci = pm->Nmesh[0] - i;
+        if(ci >= pm->Nmesh[0]) ci -= pm->Nmesh[0];
+
+        for(j = pm->ORegion.start[1]; 
+            j < pm->ORegion.start[1] + pm->ORegion.size[1]; 
+            j ++) {
+            /* always pull the gaussian from the lower quadrant plane for k = 0
+             * plane*/
+            /* always pull the whitenoise from the lower quadrant plane for k = 0
+             * plane and k == Nmesh / 2 plane*/
+            int d1 = 0, d2 = 0;
+            int cj = pm->Nmesh[1] - j;
+            if(cj >= pm->Nmesh[1]) cj -= pm->Nmesh[1];
+
+            /* d1, d2 points to the conjugate quandrant */
+            if( (ci == i && cj < j)
+             || (ci < i && cj != j)
+             || (ci < i && cj == j)) {
+                d1 = 1;
+                d2 = 1;
+            }
+
+            unsigned int seed_conj, seed_this;
+            /* the lower quadrant generator */
+            seed_conj = GETSEED(pm, seedtable, i, j, d1, d2);
+            gsl_rng_set(lower_rng, seed_conj);
+
+            seed_this = GETSEED(pm, seedtable, i, j, 0, 0);
+            gsl_rng_set(this_rng, seed_this);
+
+            for(k = 0; k <= pm->Nmesh[2] / 2; k ++) {
+                int use_conj = (d1 != 0 || d2 != 0) && (k == 0 || k == pm->Nmesh[2] / 2);
+
+                double ampl, phase;
+                if(use_conj) {
+                    /* on k = 0 and Nmesh/2 plane, we use the lower quadrant generator, 
+                     * then hermit transform the result if it is nessessary */
+                    SAMPLE(this_rng, &ampl, &phase);
+                    SAMPLE(lower_rng, &ampl, &phase);
+                } else {
+                    SAMPLE(lower_rng, &ampl, &phase);
+                    SAMPLE(this_rng, &ampl, &phase);
+                }
+
+                ptrdiff_t iabs[3] = {i, j, k};
+                ptrdiff_t ip = 0;
+                for(d = 0; d < 3; d ++) {
+                    irel[d] = iabs[d] - pm->ORegion.start[d];
+                    ip += pm->ORegion.strides[d] * irel[d];
+                }
+
+                if(irel[2] < 0) continue;
+                if(irel[2] >= pm->ORegion.size[2]) continue;
+
+                /* we want two numbers that are of std ~ 1/sqrt(2) */
+                ampl = sqrt(- log(ampl));
+
+                (delta_k + 2 * ip)[0] = ampl * cos(phase);
+                (delta_k + 2 * ip)[1] = ampl * sin(phase);
+
+                if(use_conj) {
+                    (delta_k + 2 * ip)[1] *= -1;
+                }
+
+                if((pm->Nmesh[0] - iabs[0]) % pm->Nmesh[0] == iabs[0] &&
+                   (pm->Nmesh[1] - iabs[1]) % pm->Nmesh[1] == iabs[1] &&
+                   (pm->Nmesh[2] - iabs[2]) % pm->Nmesh[2] == iabs[2]) {
+                    /* The mode is self conjuguate, thus imaginary mode must be zero */
+                    (delta_k + 2 * ip)[1] = 0;
+                    (delta_k + 2 * ip)[0] = ampl * cos(phase);
+                }
+
+                if(iabs[0] == 0 && iabs[1] == 0 && iabs[2] == 0) {
+                    /* the mean is zero */
+                    (delta_k + 2 * ip)[0] = 0;
+                    (delta_k + 2 * ip)[1] = 0;
+                }
+            }
+        }
+        gsl_rng_free(lower_rng);
+        gsl_rng_free(this_rng);
+    }
+    for(i = 0; i < 2; i ++)
+    for(j = 0; j < 2; j ++) {
+        free(seedtable[i][j]);
+    }
+/*
+    char * fn[1000];
+    sprintf(fn, "canvas.dump.f4.%d", pm->ThisTask);
+    fwrite(pm->canvas, sizeof(pm->canvas[0]), pm->ORegion.total * 2, fopen(fn, "w"));
+*/
+}
+
+/* Using fastpm's gaussian_fill for ngenic agreement. */
+static void
+gaussian_fill(PetaPMRegion * region, pfft_complex * rho_k)
+{
+    /* fastpm deals with strides properly; petapm not. So we translate it here. */
+    PM pm[1];
+    int d;
+    for (d = 0; d < 3; d ++) {
+        pm->Nmesh[d] = Nmesh;
+    }
+
+    pm->ORegion.start[0] = region->offset[2];
+    pm->ORegion.size[0] = region->size[2];
+    pm->ORegion.strides[0] = region->strides[2];
+    pm->ORegion.start[1] = region->offset[0];
+    pm->ORegion.size[1] = region->size[0];
+    pm->ORegion.strides[1] = region->strides[0];
+    pm->ORegion.start[2] = region->offset[1];
+    pm->ORegion.size[2] = region->size[1];
+    pm->ORegion.strides[2] = region->strides[1];
+
+    pm->ORegion.total = region->totalsize;
+    pm->allocsize = region->totalsize;
+    pmic_fill_gaussian_gadget(pm, (double*) rho_k, Seed);
+
+#if 0
+    /* dump the gaussian field for debugging 
+     * 
+     * the z directioin is in axis 1.
+     *
+     * */
+    FILE * rhokf = fopen("rhok", "w");
+    printf("strides %td %td %td\n", 
+            pm->ORegion.strides[0],
+            pm->ORegion.strides[1],
+            pm->ORegion.strides[2]);
+    printf("size %td %td %td\n", 
+            pm->ORegion.size[0],
+            pm->ORegion.size[1],
+            pm->ORegion.size[2]);
+    printf("offset %td %td %td\n", 
+            pm->ORegion.start[0],
+            pm->ORegion.start[1],
+            pm->ORegion.start[2]);
+    fwrite(rho_k, sizeof(double) * region->totalsize, 1, rhokf);
+    fclose(rhokf);
+#endif
+}
+
