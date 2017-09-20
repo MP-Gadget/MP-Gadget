dcb730f11c195bb87ac85b75762975b0358609e9
Yu Feng <rainwoodman@gmail.com>
2017-09-20 15:13:32 -0700

    tighten up the tests.

--- examples/travis/check-results.py
+++ examples/travis/check-results.py
@@ -1,40 +1,60 @@
 from nbodykit.lab import *
 from nbodykit.cosmology import Planck15, LinearPower
-
+import os
 from numpy.testing import assert_allclose
-
-iccat = MultipleSpeciesCatalog(['gas', 'dm'],
-        BigFileCatalog('output/IC', dataset='0/', header='Header'),
-        BigFileCatalog('output/IC', dataset='1/', header='Header'))
-
-iccat.attrs['BoxSize'] = iccat.attrs['dm.BoxSize'] * [1, 1, 1]
-
-"""
-p000 = MultipleSpeciesCatalog(['gas', 'dm'],
-        BigFileCatalog('output/PART_000', '0/', header='Header'),
-        BigFileCatalog('output/PART_000', '1/', header='Header'))
-
-p001 = MultipleSpeciesCatalog(['gas', 'dm'],
-        BigFileCatalog('output/PART_001', '0', header='Header'),
-        BigFileCatalog('output/PART_001', '1', header='Header'))
-"""
-
-print(iccat.attrs)
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 
-fig = Figure(figsize=(5, 5), dpi=200)
-canvas = FigureCanvasAgg(fig)
-ax = fig.add_subplot(111)
 
-pklin = LinearPower(Planck15, redshift=1 / iccat.attrs['dm.Time'] - 1)
+def compute_power(output):
 
-icp = FFTPower(iccat['dm'], mode='1d', Nmesh=128)
-ax.plot(icp.power['k'], icp.power['power'], label='data')
-ax.plot(icp.power['k'], pklin(icp.power['k']), label='model')
-ax.loglog()
+    cat = MultipleSpeciesCatalog(['gas', 'dm'],
+            BigFileCatalog(output, dataset='0/', header='Header'),
+            BigFileCatalog(output, dataset='1/', header='Header'))
 
-fig.savefig('ic.png')
-print(abs(icp.power['power'])[:10], pklin(icp.power['k'])[:10])
-assert_allclose(abs(icp.power['power'])[:10], pklin(icp.power['k'])[:10], rtol=0.1, atol=0.1)
+    cat.attrs['BoxSize'] = cat.attrs['dm.BoxSize'] * [1, 1, 1]
+
+    pksim = FFTPower(cat, mode='1d', Nmesh=128)
+#    pksimgas = FFTPower(cat['gas'], mode='1d', Nmesh=128)
+#    pksimdm = FFTPower(cat['dm'], mode='1d', Nmesh=128)
+    z = 1. / cat.attrs['dm.Time'] - 1
+    return pksim, z
+
+ref, ref_z = compute_power('output/IC')
+
+def test_power(output, ref, ref_z):
+    pksim, z = compute_power(output)
+
+    fig = Figure(figsize=(5, 5), dpi=200)
+    canvas = FigureCanvasAgg(fig)
+    ax = fig.add_subplot(111)
+
+    pklin = LinearPower(Planck15, redshift=z)
+
+    D = Planck15.scale_independent_growth_factor(z) / Planck15.scale_independent_growth_factor(ref_z)
+
+    ax.plot(pksim.power['k'], pksim.power['power'] / pklin(pksim.power['k']), label=output)
+    ax.plot(ref.power['k'], ref.power['power'] * D ** 2 / pklin(ref.power['k']), label='ref, linearly grown')
+    ax.set_xscale('log')
+    ax.set_ylim(0, 2)
+    ax.legend()
+    fig.savefig(os.path.basename(output) + '.png')
+
+    # asserting the linear growth is 1% accurate
+    assert_allclose(abs(pksim.power['power'])[2:8],
+                    abs(ref.power['power'])[2:8] * D ** 2,
+                        rtol=0.01, atol=0.0)
+
+
+# asserting the initial power spectrum is 5% accurate
+pklin = LinearPower(Planck15, redshift=ref_z)
+assert_allclose(abs(ref.power['power'])[2:8],
+                abs(pklin(ref.power['k']))[2:8],
+                rtol=0.05, atol=0.0)
+
+test_power('output/IC', ref, ref_z)
+
+test_power('output/PART_000', ref, ref_z)
+test_power('output/PART_001', ref, ref_z)
+test_power('output/PART_002', ref, ref_z)
