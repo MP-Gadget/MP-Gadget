1a4ffcaec62570ea266762263de8be321ac86f0f
Yu Feng <rainwoodman@gmail.com>
2017-10-17 14:35:56 -0700

    save MinPotPos and MinPotVel for restarting.

--- petaio.c
+++ petaio.c
@@ -722,6 +722,8 @@ SIMPLE_PROPERTY_TYPE(StarFormationTime, 5, BHP(i).FormationTime, float, 1)
 SIMPLE_PROPERTY(BlackholeMass, BHP(i).Mass, float, 1)
 SIMPLE_PROPERTY(BlackholeAccretionRate, BHP(i).Mdot, float, 1)
 SIMPLE_PROPERTY(BlackholeProgenitors, BHP(i).CountProgs, float, 1)
+SIMPLE_PROPERTY(BlackholeMinPotPos, BHP(i).MinPotPos[0], double, 3)
+SIMPLE_PROPERTY(BlackholeMinPotVel, BHP(i).MinPotVel[0], float, 3)
 #endif
 /*This is only used if FoF is enabled*/
 SIMPLE_GETTER(GTGroupID, P[i].GrNr, uint32_t, 1)
@@ -806,6 +808,8 @@ static void register_io_blocks() {
     IO_REG(BlackholeMass,          "f4", 1, 5);
     IO_REG(BlackholeAccretionRate, "f4", 1, 5);
     IO_REG(BlackholeProgenitors,   "i4", 1, 5);
+    IO_REG(BlackholeMinPotPos, "f8", 3, 5);
+    IO_REG(BlackholeMinPotVel,   "f4", 3, 5);
 #endif
     if(All.SnapshotWithFOF)
         fof_register_io_blocks();
