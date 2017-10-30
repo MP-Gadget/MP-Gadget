680d8d69adb3776416719d3adb339bef168304e9
Yu Feng <yfeng1@berkeley.edu>
2017-10-30 13:40:08 -0700

    rename ierr to where.
    >0 for everywhere, <=0 for root only.
    
    Perhaps need some enum but requires changing too many places;
    better to defer that till all PRs are resolved.

--- endrun.h
+++ endrun.h
@@ -1,3 +1,3 @@
 
-void endrun(int, const char * fmt, ...);
-void message(int, const char * fmt, ...);
+void endrun(int where, const char * fmt, ...);
+void message(int where, const char * fmt, ...);
