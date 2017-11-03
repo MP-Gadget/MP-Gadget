9798ee544429944daa730e103badcbb8b91db5f5
Yu Feng <rainwoodman@gmail.com>
2017-11-03 15:40:00 -0700

    forgot files!

---
+++ event.c
@@ -0,0 +1,53 @@
+#include "event.h"
+#include <stdlib.h>
+#include <string.h>
+
+int
+event_listen(EventSpec * eh, eventfunc func, void * userdata)
+{
+    int i;
+    for(i = 0; i < eh->used; i ++) {
+        if(eh->h[i].func == func && eh->h[i].userdata == userdata) {
+            return 0;
+        }
+    }
+    if(eh->used == MAXEH) {
+        /* overflown */
+        abort();
+    }
+
+    eh->h[eh->used].func = func;
+    eh->h[eh->used].userdata = userdata;
+    eh->used ++;
+    return 0;
+}
+
+int
+event_unlisten(EventSpec * eh, eventfunc func, void * userdata)
+{
+    int i;
+    for(i = 0; i < eh->used; i ++) {
+        if(eh->h[i].func == func && eh->h[i].userdata == userdata) {
+            break;
+        }
+    }
+    if(i == eh->used) {
+        return 1;
+    }
+    memmove(&eh->h[i], &eh->h[i+1], sizeof(eh->h[0]) * (eh->used - i - 1));
+    eh->used --;
+    return 0;
+}
+
+
+int
+event_emit(EventSpec * eh, EIBase * event)
+{
+
+    int i;
+    for(i = 0; i < eh->used; i ++) {
+        eh->h[i].func(event, eh->h[i].userdata);
+    }
+    return 0;
+}
+
---
+++ event.h
@@ -0,0 +1,38 @@
+#ifndef _EVENT_H_
+#define _EVENT_H_
+
+
+typedef struct EventHandler EventHandler;
+
+typedef struct EventSpec EventSpec;
+typedef struct {
+    int unused;
+} EIBase;
+
+typedef int (*eventfunc) (EIBase * event, void * userdata);
+
+struct EventHandler
+{
+    eventfunc func;
+    void * userdata;
+};
+
+#define MAXEH 8
+
+struct EventSpec
+{
+    char name[32];
+    int used;
+    EventHandler h[MAXEH];
+};
+
+int
+event_listen(EventSpec * e, eventfunc func, void * userdata);
+
+int
+event_unlisten(EventSpec * e, eventfunc func, void * userdata);
+
+int
+event_emit(EventSpec * eh, EIBase * event);
+
+#endif
