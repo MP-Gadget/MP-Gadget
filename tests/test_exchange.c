4f2d488592c700ae95d7087ad5386ffed3e7ec04
Simeon Bird <sbird4@jhu.edu>
2017-11-14 14:35:38 -0500

    Add a simple test of SPH/slots exchange

--- tests/test_exchange.c
+++ tests/test_exchange.c
@@ -55,13 +55,7 @@ int layoutfunc(int p)
     return (NTask * P[p].Pos[2])/ All.BoxSize;
 }
 
-static int setup_exchange(void **state) {
-    /*Set up the important parts of the All structure.*/
-    /*Particles should not be outside this*/
-    return 0;
-}
-
-static void do_exchange_test(int numpart)
+static void do_exchange_test(int numpart, int garbage)
 {
     int i;
     All.MaxPart = numpart;
@@ -73,10 +67,18 @@ static void do_exchange_test(int numpart)
     end = MPI_Wtime();
     double ms = (end - start)*1000;
     message(0,"Exchange in %.3g ms\n", ms);
-    assert_int_equal(NumPart, numpart);
-    for(i=0; i<numpart; i++) {
+    for(i=0; i<NumPart; i++) {
 //         message(1, "i = %d, pos = %g %g %g\n",i, P[i].Pos[0], P[i].Pos[1], P[i].Pos[2]);
         assert_int_equal(layoutfunc(i), ThisTask);
+        /*Check first N/NTask elements are from this processor*/
+        if(i < numpart/NTask - garbage)
+            assert_int_equal(P[i].ID/All.MaxPart, ThisTask);
+        else
+            assert_int_not_equal(P[i].ID/All.MaxPart, ThisTask);
+        /*Check there was no corruption*/
+        assert_true(P[i].Pos[0] >= 0 && P[i].Pos[0] < All.BoxSize);
+        assert_true(P[i].Pos[1] >= 0 && P[i].Pos[1] < All.BoxSize);
+        assert_true(P[i].Pos[2] >= 0 && P[i].Pos[2] < All.BoxSize);
     }
 }
 
@@ -96,19 +98,69 @@ static void exc_onlydm(void ** state) {
     #pragma omp parallel for
     for(i=0; i<All.MaxPart; i++) {
         P[i].Type = 1;
+        P[i].ID = ThisTask*All.MaxPart + i;
         P[i].Pos[0] = (All.BoxSize/ncbrt/NTask) * (ThisTask + (i/ncbrt/ncbrt));
         P[i].Pos[1] = (All.BoxSize/ncbrt) * ((i/ncbrt) % ncbrt);
         P[i].Pos[2] = (All.BoxSize/ncbrt) * (i % ncbrt);
     }
-    do_exchange_test(All.MaxPart);
+    do_exchange_test(All.MaxPart, 0);
+    assert_int_equal(NumPart, All.MaxPart);
+    free(P);
+}
+
+static void exc_sph(void ** state) {
+    /*Set up the particle data*/
+    All.BoxSize = 8;
+    int ncbrt = 96;
+    All.MaxPart = ncbrt*ncbrt*ncbrt;
+    NumPart = ncbrt*ncbrt*ncbrt;
+    N_sph_slots = All.MaxPart/2;
+    /*This is different from MaxPart to make sure we test*/
+    All.MaxPartSph = All.MaxPart/2;
+    All.MaxPartBh = 0;
+    All.MaxPartStar = 0;
+    P = calloc(All.MaxPart, sizeof(struct particle_data));
+    SphP = calloc(All.MaxPart/2,sizeof(struct sph_particle_data));
+    /* Create a regular grid of particles, 8x8x8, all of type 1,
+     * in a box 8 kpc across.*/
+    int i;
+    #pragma omp parallel for
+    for(i=0; i<All.MaxPart/2; i++) {
+        P[i].Type = 1;
+        P[i].ID = ThisTask*All.MaxPart + i;
+        P[i].Pos[0] = (All.BoxSize/ncbrt/NTask) * (ThisTask + (i/ncbrt/ncbrt));
+        P[i].Pos[1] = (All.BoxSize/ncbrt) * ((i/ncbrt) % ncbrt);
+        P[i].Pos[2] = (All.BoxSize/ncbrt) * (i % ncbrt);
+    }
+    /* This means that the SPH particles will all end up on different processors.*/
+    #pragma omp parallel for
+    for(i=All.MaxPart/2; i<All.MaxPart; i++) {
+        P[i].Type = 0;
+        P[i].ID = ThisTask*All.MaxPart + i;
+        P[i].Pos[0] = (All.BoxSize/ncbrt/NTask) * (ThisTask + (i/ncbrt/ncbrt));
+        P[i].Pos[1] = (All.BoxSize/ncbrt) * ((i/ncbrt) % ncbrt);
+        P[i].Pos[2] = (All.BoxSize/ncbrt) * (i % ncbrt);
+        P[i].PI = i - All.MaxPart/2;
+        SPHP(i).base.ID = P[i].ID;
+    }
+
+    do_exchange_test(All.MaxPart,0);
+    for(i=0; i<NumPart; i++) {
+        assert_true(P[i].ID % All.MaxPart < All.MaxPart);
+        if(P[i].Type != 1)
+            assert_int_equal(P[i].ID , SPHP(i).base.ID);
+    }
+    assert_int_equal(NumPart, All.MaxPart);
+    assert_int_equal(N_sph_slots, All.MaxPart/2);
+
     free(P);
 }
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(exc_onlydm),
-//         cmocka_unit_test(exchange_sph),
+        cmocka_unit_test(exc_sph),
 //         cmocka_unit_test(exchange_all),
     };
-    return cmocka_run_group_tests_mpi(tests, setup_exchange, NULL);
+    return cmocka_run_group_tests_mpi(tests, NULL, NULL);
 }
