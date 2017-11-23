4079ff9157d4d522c5bc1bfff601c09498c4136b
Simeon Bird <sbird4@jhu.edu>
2017-11-23 08:49:30 -0500

    Clean up includes

--- blackhole.c
+++ blackhole.c
@@ -16,8 +16,6 @@
 #include "system.h"
 #include "fof.h"
 #include "blackhole.h"
-#include "forcetree.h"
-#include "sfr_eff.h"
 #include "timestep.h"
 /*! \file blackhole.c
  *  \brief routines for gas accretion onto black holes, and black hole mergers
