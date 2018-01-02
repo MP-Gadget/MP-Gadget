4c6da736a7f26797c662646fded121e20dab7c56
Yu Feng <rainwoodman@gmail.com>
2018-01-01 22:04:15 -0800

    set all blackholes to at least bin 10.

--- timestep.c
+++ timestep.c
@@ -440,6 +440,8 @@ int get_timestep(int p,		/*!< particle index */
             if(dt_accr < dt)
                 dt = dt_accr;
         }
+        /* XXX : temporarily set all balckhols to at least bin 10. */
+        BHP(p).TimeBinLimit = 10;
         if(BHP(p).TimeBinLimit > 0) {
             double dt_limiter = (1L << BHP(p).TimeBinLimit) * All.Timebase_interval / All.cf.hubble;
             if (dt_limiter < dt) dt = dt_limiter;
