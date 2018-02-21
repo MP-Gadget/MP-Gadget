0b3f0122476f98d53f683eea9b5993bdf8852999
Yu Feng <rainwoodman@gmail.com>
2018-02-21 08:30:52 -0800

    add utils.h

---
+++ libgadget/utils.h
@@ -0,0 +1,23 @@
+/**
+ *
+ * libgadget-utils.a contains the porting of MP-Gadget that have
+ * nothing to do with cosmological simulations.
+ *
+ */
+#ifndef GADGET_UTILS_H
+#define GADGET_UTILS_H
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <mpi.h>
+#include "utils/peano.h"
+#include "utils/memory.h"
+#include "utils/mymalloc.h"
+#include "utils/paramset.h"
+#include "utils/event.h"
+#include "utils/openmpsort.h"
+#include "utils/system.h"
+#include "utils/string.h"
+#include "utils/endrun.h"
+#include "utils/interp.h"
+#endif
