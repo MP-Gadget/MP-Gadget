416f596c82157e363db07bbf06999d0573c6ac03
Simeon Bird <sbird@ucr.edu>
2018-03-18 20:19:10 -0700

    Set InitGasTemp to the CMB temperature by default

--- gadget/params.c
+++ gadget/params.c
@@ -199,7 +199,7 @@ create_gadget_parameter_set()
     param_declare_double(ps, "PartAllocFactor", REQUIRED, 0, "");
     param_declare_double(ps, "TopNodeAllocFactor", OPTIONAL, 0.5, "");
 
-    param_declare_double(ps, "InitGasTemp", OPTIONAL, 300, "Initial gas temperature");
+    param_declare_double(ps, "InitGasTemp", OPTIONAL, -1, "Initial gas temperature. By default set to CMB temperature at starting redshift.");
     param_declare_double(ps, "MinGasTemp", OPTIONAL, 5, "Minimum gas temperature");
 
     param_declare_int(ps, "SnapshotWithFOF", REQUIRED, 0, "Enable Friends-of-Friends halo finder.");
--- libgadget/run.c
+++ libgadget/run.c
@@ -468,6 +468,9 @@ set_units(void)
 
     All.CP.Hubble = HUBBLE * All.UnitTime_in_s;
     init_cosmology(&All.CP, All.TimeInit, All.BoxSize * All.UnitLength_in_cm);
+
+    if(All.InitGasTemp < 0)
+        All.InitGasTemp = DMAX(All.MinGasTemp, All.CP.CMBTemperature / All.TimeInit);
     /*Initialise the hybrid neutrinos, after Omega_nu*/
     if(All.HybridNeutrinosOn)
         init_hybrid_nu(&All.CP.ONu.hybnu, All.CP.MNu, All.HybridVcrit, C/1e5, All.HybridNuPartTime, All.CP.ONu.kBtnu);
