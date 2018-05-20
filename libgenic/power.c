weylwong/MP-Gadget3

Add code for MP-GenIC to load transfer functions at different redshifts, and differentiate them to get a velocity growth function. There are new config parameters to control this. As yet, nothing is done with the velocity growth function.

libgenic/power.c
@@ -23,8 +23,9 @@ static double PrimordialIndex;
 static double UnitLength_in_cm;
 static Cosmology * CP;
 
-#define MAXCOLS 4
+#define MAXCOLS 5
 
+/*Columns: 0 == baryon, 1 == CDM, 2 == neutrino, 3 == CDM + baryons, 4 == total*/
 struct table
 {
     int Nentry;
@@ -35,6 +36,7 @@ struct table
 };
 static struct table power_table;
 static struct table transfer_table;
+static struct table growth_table;
 
 double PowerSpec(double k, int Type)
 {
@@ -49,6 +51,23 @@ double PowerSpec(double k, int Type)
   power *= Norm;
 
   return power;
+}
+
+double dlogGrowth(double kmag, int Type)
+{
+  const double logk = log10(kmag);
+
+  if(logk < growth_table.logk[0] || logk > growth_table.logk[growth_table.Nentry - 1])
+      return 1;
+
+  /*Default to total*/
+  if(Type < 0 || Type >= MAXCOLS)
+      Type = MAXCOLS-1;
+
+  double growth =  gsl_interp_eval(growth_table.mat_intp[Type], growth_table.logk, growth_table.logD[Type], logk, growth_table.mat_intp_acc[Type]);
+  if(isinf(growth) || isnan(growth) || growth < 0)
+      endrun(1,"Growth function is: %g for k = %g, Type = %d\n",growth, kmag, Type);
+  return growth;
 }
 
 void parse_power(int i, double k, char * line, struct table *out_tab, int * InputInLog10, double scale)
@@ -94,10 +113,11 @@ void parse_transfer(int i, double k, char * line, struct table *out_tab, int * I
      * In the input CAMB file 0 is CDM, 1 is baryons, 2 is photons,
      * 3 is massless neutrinos, 4 is massive neutrinos, 5 is total,
      * 6 is cdm + baryons.*/
-    out_tab->logD[0][i] = pow(transfers[1]/transfers[5],2);
-    out_tab->logD[1][i] = pow(transfers[0]/transfers[5],2);
-    out_tab->logD[2][i] = pow(transfers[4]/transfers[5],2);
-    out_tab->logD[3][i] = pow(transfers[6]/transfers[5],2);
+    out_tab->logD[0][i] = transfers[1];
+    out_tab->logD[1][i] = transfers[0];
+    out_tab->logD[2][i] = transfers[4];
+    out_tab->logD[3][i] = transfers[6];
+    out_tab->logD[4][i] = transfers[5];
 }
 
 void read_power_table(int ThisTask, const char * inputfile, const int ncols, struct table * out_tab, double scale, void (*parse_line)(int i, double k, char * line, struct table *, int *InputInLog10, double scale))
@@ -160,24 +180,72 @@ void read_power_table(int ThisTask, const char * inputfile, const int ncols, str
     for(j=0; j<ncols; j++) {
         out_tab->mat_intp[j] = gsl_interp_alloc(gsl_interp_cspline,out_tab->Nentry);
         out_tab->mat_intp_acc[j] = gsl_interp_accel_alloc();
-        gsl_interp_init(out_tab->mat_intp[j],out_tab->logk, out_tab->logD[j],out_tab->Nentry);
     }
+}
+
+int
+initialise_transfer_table(int ThisTask, double InitTime, struct power_params * ppar)
+{
+    int i, t;
+    if(strlen(ppar->FileWithTransferFunction) > 0)
+        read_power_table(ThisTask, ppar->FileWithTransferFunction, MAXCOLS, &transfer_table, ppar->SpectrumLengthScale, parse_transfer);
+    if(strlen(ppar->FileWithFutureTransferFunction) > 0) {
+        double meangrowth[MAXCOLS] = {0};
+        /*Read and build growth table*/
+        read_power_table(ThisTask, ppar->FileWithFutureTransferFunction, MAXCOLS, &growth_table, ppar->SpectrumLengthScale, parse_transfer);
+        if(growth_table.Nentry != transfer_table.Nentry)
+            endrun(1,"Transfer tables differ in number of entries: %d %d\n", growth_table.Nentry, transfer_table.Nentry);
+        /*Numerically differentiate the transfer functions*/
+        double afut = 1/(1.+ppar->InputFutureRedshift);
+        double dloga = log(afut) - log(InitTime);
+        /*This is d log D[type] / d log a*/
+        for(t = 0; t < MAXCOLS; t++) {
+        int nmean=0;
+            for(i=0; i< growth_table.Nentry; i++) {
+                if(growth_table.logD[t][i] > 0)
+                    growth_table.logD[t][i] = (log(growth_table.logD[t][i]) - log(transfer_table.logD[t][i]))/dloga;
+                if(growth_table.logk[i] > power_table.logk[0]) {
+                    meangrowth[t] += growth_table.logD[t][i];
+                    nmean++;
+                }
+            }
+            gsl_interp_init(growth_table.mat_intp[t],growth_table.logk, growth_table.logD[t],growth_table.Nentry);
+        meangrowth[t]/= nmean;
+        }
+        message(0,"Scale-dependent growth calculated. Mean = %g %g %g %g\n",meangrowth[0], meangrowth[1], meangrowth[2], meangrowth[3]);
+    }
+    if(transfer_table.Nentry == 0 || growth_table.Nentry == 0) {
+        endrun(1, "Could not read both transfer tables at: '%s' or '%s'\n",ppar->FileWithFutureTransferFunction, ppar->FileWithTransferFunction);
+    }
+
+    /*Transform T(k) to to T(k)/T_tot(k)^2*/
+    for(t = 0; t < MAXCOLS-1; t++) {
+        for(i=0; i< transfer_table.Nentry; i++)
+            transfer_table.logD[t][i] = transfer_table.logD[t][i]/transfer_table.logD[MAXCOLS-1][i];
+        gsl_interp_init(transfer_table.mat_intp[t],transfer_table.logk, transfer_table.logD[t],transfer_table.Nentry);
+    }
+    message(0, "Power spectrum rows: %d, Transfer: %d (%g -> %g) (second transfer: %d)\n", power_table.Nentry, transfer_table.Nentry, growth_table.Nentry, transfer_table.logD[0][0],transfer_table.logD[0][transfer_table.Nentry-1]);
+    return transfer_table.Nentry;
 }
 
 int initialize_powerspectrum(int ThisTask, double InitTime, double UnitLength_in_cm_in, Cosmology * CPin, struct power_params * ppar)
 {
-    if(ppar->WhichSpectrum == 2) {
-        read_power_table(ThisTask, ppar->FileWithInputSpectrum, 1, &power_table, ppar->SpectrumLengthScale, parse_power);
-        transfer_table.Nentry = 0;
-        if(strlen(ppar->FileWithTransferFunction) > 0)
-            read_power_table(ThisTask, ppar->FileWithTransferFunction, MAXCOLS, &transfer_table, ppar->SpectrumLengthScale, parse_transfer);
-        message(0, "Power spectrum rows: %d, Transfer: %d\n", power_table.Nentry, transfer_table.Nentry);
-    }
     WhichSpectrum = ppar->WhichSpectrum;
     /*Used only for tk_eh*/
     PrimordialIndex = ppar->PrimordialIndex;
     UnitLength_in_cm = UnitLength_in_cm_in;
     CP = CPin;
+
+    if(ppar->WhichSpectrum == 2) {
+        read_power_table(ThisTask, ppar->FileWithInputSpectrum, 1, &power_table, ppar->SpectrumLengthScale, parse_power);
+        /*Initialise the interpolator*/
+        gsl_interp_init(power_table.mat_intp[0],power_table.logk, power_table.logD[0],power_table.Nentry);
+        transfer_table.Nentry = 0;
+        growth_table.Nentry = 0;
+        if(ppar->DifferentTransferFunctions || ppar->ScaleDepVelocity) {
+            initialise_transfer_table(ThisTask, InitTime, ppar);
+        }
+    }
 
     Norm = 1.0;
     if (ppar->Sigma8 > 0) {
@@ -204,11 +272,13 @@ double PowerSpec_Tabulated(double k, int Type)
   const double logD = gsl_interp_eval(power_table.mat_intp[0], power_table.logk, power_table.logD[0], logk, power_table.mat_intp_acc[0]);
   double trans = 1;
   /*Transfer table stores (T_type(k) / T_tot(k))^2*/
-  if(Type >= 0 && Type < MAXCOLS && transfer_table.Nentry > 0)
+  if(Type >= 0 && Type < MAXCOLS-1 && transfer_table.Nentry > 0)
       trans = gsl_interp_eval(transfer_table.mat_intp[Type], transfer_table.logk, transfer_table.logD[Type], logk, transfer_table.mat_intp_acc[Type]);
 
-  double power = pow(10.0, logD) * trans;
+  double power = pow(10.0, logD) * trans * trans;
 
+  if(isinf(power) || isnan(power) || power < 0)
+      endrun(1,"Power spectrum is: %g for k = %g, Type = %d (tk = %g, logD = %g)\n",power, k, Type, trans, logD);
   return power;
 }
 

MP-Gadget/MP-Gadget

libgenic/power.c
@@ -23,8 +23,9 @@ static double PrimordialIndex;
 static double UnitLength_in_cm;
 static Cosmology * CP;
 
-#define MAXCOLS 4
+#define MAXCOLS 5
 
+/*Columns: 0 == baryon, 1 == CDM, 2 == neutrino, 3 == CDM + baryons, 4 == total*/
 struct table
 {
     int Nentry;
@@ -35,6 +36,7 @@ struct table
 };
 static struct table power_table;
 static struct table transfer_table;
+static struct table growth_table;
 
 double DeltaSpec(double k, int Type)
 {
@@ -49,6 +51,23 @@ double DeltaSpec(double k, int Type)
   power *= Norm;
 
   return power;
+}
+
+double dlogGrowth(double kmag, int Type)
+{
+  const double logk = log10(kmag);
+
+  if(logk < growth_table.logk[0] || logk > growth_table.logk[growth_table.Nentry - 1])
+      return 1;
+
+  /*Default to total*/
+  if(Type < 0 || Type >= MAXCOLS)
+      Type = MAXCOLS-1;
+
+  double growth =  gsl_interp_eval(growth_table.mat_intp[Type], growth_table.logk, growth_table.logD[Type], logk, growth_table.mat_intp_acc[Type]);
+  if(isinf(growth) || isnan(growth) || growth < 0)
+      endrun(1,"Growth function is: %g for k = %g, Type = %d\n",growth, kmag, Type);
+  return growth;
 }
 
 void parse_power(int i, double k, char * line, struct table *out_tab, int * InputInLog10, double scale)
@@ -95,10 +114,11 @@ void parse_transfer(int i, double k, char * line, struct table *out_tab, int * I
      * In the input CAMB file 0 is CDM, 1 is baryons, 2 is photons,
      * 3 is massless neutrinos, 4 is massive neutrinos, 5 is total,
      * 6 is cdm + baryons.*/
-    out_tab->logD[0][i] = transfers[1]/transfers[5];
-    out_tab->logD[1][i] = transfers[0]/transfers[5];
-    out_tab->logD[2][i] = transfers[4]/transfers[5];
-    out_tab->logD[3][i] = transfers[6]/transfers[5];
+    out_tab->logD[0][i] = transfers[1];
+    out_tab->logD[1][i] = transfers[0];
+    out_tab->logD[2][i] = transfers[4];
+    out_tab->logD[3][i] = transfers[6];
+    out_tab->logD[4][i] = transfers[5];
 }
 
 void read_power_table(int ThisTask, const char * inputfile, const int ncols, struct table * out_tab, double scale, void (*parse_line)(int i, double k, char * line, struct table *, int *InputInLog10, double scale))
@@ -161,24 +181,72 @@ void read_power_table(int ThisTask, const char * inputfile, const int ncols, str
     for(j=0; j<ncols; j++) {
         out_tab->mat_intp[j] = gsl_interp_alloc(gsl_interp_cspline,out_tab->Nentry);
         out_tab->mat_intp_acc[j] = gsl_interp_accel_alloc();
-        gsl_interp_init(out_tab->mat_intp[j],out_tab->logk, out_tab->logD[j],out_tab->Nentry);
     }
+}
+
+int
+initialise_transfer_table(int ThisTask, double InitTime, struct power_params * ppar)
+{
+    int i, t;
+    if(strlen(ppar->FileWithTransferFunction) > 0)
+        read_power_table(ThisTask, ppar->FileWithTransferFunction, MAXCOLS, &transfer_table, ppar->SpectrumLengthScale, parse_transfer);
+    if(strlen(ppar->FileWithFutureTransferFunction) > 0) {
+        double meangrowth[MAXCOLS] = {0};
+        /*Read and build growth table*/
+        read_power_table(ThisTask, ppar->FileWithFutureTransferFunction, MAXCOLS, &growth_table, ppar->SpectrumLengthScale, parse_transfer);
+        if(growth_table.Nentry != transfer_table.Nentry)
+            endrun(1,"Transfer tables differ in number of entries: %d %d\n", growth_table.Nentry, transfer_table.Nentry);
+        /*Numerically differentiate the transfer functions*/
+        double afut = 1/(1.+ppar->InputFutureRedshift);
+        double dloga = log(afut) - log(InitTime);
+        /*This is d log D[type] / d log a*/
+        for(t = 0; t < MAXCOLS; t++) {
+        int nmean=0;
+            for(i=0; i< growth_table.Nentry; i++) {
+                if(growth_table.logD[t][i] > 0)
+                    growth_table.logD[t][i] = (log(growth_table.logD[t][i]) - log(transfer_table.logD[t][i]))/dloga;
+                if(growth_table.logk[i] > power_table.logk[0]) {
+                    meangrowth[t] += growth_table.logD[t][i];
+                    nmean++;
+                }
+            }
+            gsl_interp_init(growth_table.mat_intp[t],growth_table.logk, growth_table.logD[t],growth_table.Nentry);
+        meangrowth[t]/= nmean;
+        }
+        message(0,"Scale-dependent growth calculated. Mean = %g %g %g %g\n",meangrowth[0], meangrowth[1], meangrowth[2], meangrowth[3]);
+    }
+    if(transfer_table.Nentry == 0 || growth_table.Nentry == 0) {
+        endrun(1, "Could not read both transfer tables at: '%s' or '%s'\n",ppar->FileWithFutureTransferFunction, ppar->FileWithTransferFunction);
+    }
+
+    /*Transform T(k) to to T(k)/T_tot(k)*/
+    for(t = 0; t < MAXCOLS-1; t++) {
+        for(i=0; i< transfer_table.Nentry; i++)
+            transfer_table.logD[t][i] = transfer_table.logD[t][i]/transfer_table.logD[MAXCOLS-1][i];
+        gsl_interp_init(transfer_table.mat_intp[t],transfer_table.logk, transfer_table.logD[t],transfer_table.Nentry);
+    }
+    message(0, "Power spectrum rows: %d, Transfer: %d (%g -> %g) (second transfer: %d)\n", power_table.Nentry, transfer_table.Nentry, growth_table.Nentry, transfer_table.logD[0][0],transfer_table.logD[0][transfer_table.Nentry-1]);
+    return transfer_table.Nentry;
 }
 
 int initialize_powerspectrum(int ThisTask, double InitTime, double UnitLength_in_cm_in, Cosmology * CPin, struct power_params * ppar)
 {
-    if(ppar->WhichSpectrum == 2) {
-        read_power_table(ThisTask, ppar->FileWithInputSpectrum, 1, &power_table, ppar->SpectrumLengthScale, parse_power);
-        transfer_table.Nentry = 0;
-        if(strlen(ppar->FileWithTransferFunction) > 0)
-            read_power_table(ThisTask, ppar->FileWithTransferFunction, MAXCOLS, &transfer_table, ppar->SpectrumLengthScale, parse_transfer);
-        message(0, "Power spectrum rows: %d, Transfer: %d\n", power_table.Nentry, transfer_table.Nentry);
-    }
     WhichSpectrum = ppar->WhichSpectrum;
     /*Used only for tk_eh*/
     PrimordialIndex = ppar->PrimordialIndex;
     UnitLength_in_cm = UnitLength_in_cm_in;
     CP = CPin;
+
+    if(ppar->WhichSpectrum == 2) {
+        read_power_table(ThisTask, ppar->FileWithInputSpectrum, 1, &power_table, ppar->SpectrumLengthScale, parse_power);
+        /*Initialise the interpolator*/
+        gsl_interp_init(power_table.mat_intp[0],power_table.logk, power_table.logD[0],power_table.Nentry);
+        transfer_table.Nentry = 0;
+        growth_table.Nentry = 0;
+        if(ppar->DifferentTransferFunctions || ppar->ScaleDepVelocity) {
+            initialise_transfer_table(ThisTask, InitTime, ppar);
+        }
+    }
 
     Norm = 1.0;
     if (ppar->Sigma8 > 0) {
@@ -205,12 +273,14 @@ double Delta_Tabulated(double k, int Type)
   const double logD = gsl_interp_eval(power_table.mat_intp[0], power_table.logk, power_table.logD[0], logk, power_table.mat_intp_acc[0]);
   double trans = 1;
   /*Transfer table stores (T_type(k) / T_tot(k))^2*/
-  if(Type >= 0 && Type < MAXCOLS && transfer_table.Nentry > 0) {
+  if(Type >= 0 && Type < MAXCOLS-1 && transfer_table.Nentry > 0) {
       trans = gsl_interp_eval(transfer_table.mat_intp[Type], transfer_table.logk, transfer_table.logD[Type], logk, transfer_table.mat_intp_acc[Type]);
   }
 
   double delta = pow(10.0, logD) * trans;
 
+  if(!isfinite(delta))
+      endrun(1,"Power spectrum is: %g for k = %g, Type = %d (tk = %g, logD = %g)\n",delta, k, Type, trans, logD);
   return delta;
 }
 