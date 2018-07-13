1d8e7ddd45ed110f5788fd8886b32cf8f3666737
Simeon Bird <sbird@ucr.edu>
2018-07-13 09:20:48 -0700

    Make sure PM step is not longer than the longest particle timestep, fixing a rare edge case in the timestep code.
    If the PM step was longer than the longest particle timestep, and
    some of the particles in the longest timestep dropped to a longer
    timestep, they could fail to be active on the next PM step, causing the
    integer timeline to get out of sync. Fix this by enforcing that the PM
    step is never longer than the longest non-empty timestep.

--- libgadget/timestep.c
+++ libgadget/timestep.c
@@ -142,7 +142,8 @@ find_timesteps(int * MinTimeBin)
         reverse_and_apply_gravity();
 
     /*Update the PM timestep size */
-    if(is_PM_timestep(All.Ti_Current)) {
+    const int isPM = is_PM_timestep(All.Ti_Current);
+    if(isPM) {
         SyncPoint * next = find_next_sync_point(All.Ti_Current);
         inttime_t dti_max;
         if(next == NULL) {
@@ -176,8 +177,8 @@ find_timesteps(int * MinTimeBin)
     }
 
     int badstepsizecount = 0;
-    int mTimeBin = TIMEBINS;
-    #pragma omp parallel for reduction(min: mTimeBin) reduction(+: badstepsizecount)
+    int mTimeBin = TIMEBINS, maxTimeBin = 0;
+    #pragma omp parallel for reduction(min: mTimeBin) reduction(+: badstepsizecount) reduction(max:maxTimeBin)
     for(pa = 0; pa < NumActiveParticle; pa++)
     {
         const int i = get_active_particle(pa);
@@ -214,16 +215,25 @@ find_timesteps(int * MinTimeBin)
         }
         /* This moves particles between time bins:
          * active particles always remain active
-         * until reconstruct_timebins is called
-         * (during domain, on new timestep).*/
+         * until rebuild_activelist is called
+         * (after domain, on new timestep).*/
         P[i].TimeBin = bin;
+        /*Find max and min*/
         if(bin < mTimeBin)
             mTimeBin = bin;
+        if(bin > maxTimeBin)
+            maxTimeBin = bin;
     }
 
     MPI_Allreduce(MPI_IN_PLACE, &badstepsizecount, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(MPI_IN_PLACE, &mTimeBin, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &maxTimeBin, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 
+    /* Ensure that the PM timestep is not longer than the longest tree timestep;
+     * this prevents particles in the longest timestep being active and moving into a higher bin
+     * between PM timesteps, thus skipping the PM step entirely.*/
+    if(isPM && PM.length > dti_from_timebin(maxTimeBin))
+        PM.length = dti_from_timebin(maxTimeBin);
     if(badstepsizecount) {
         message(0, "bad timestep spotted: terminating and saving snapshot.\n");
         dump_snapshot();
