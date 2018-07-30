19733c5ddf529504d8785e10eb6f277aff25f487
Simeon Bird <sbird@ucr.edu>
2018-07-29 20:09:30 -0700

    Add some comments to the force kernel generation script and tidy it up a bit

--- tools/generate-force-kernels.py
+++ tools/generate-force-kernels.py
@@ -1,21 +1,28 @@
-from pmesh.pm import ParticleMesh
+"""Script to generate the short-range force kernel tables for Gadget.
+These have to account for smoothing on large scales to match directly onto the PM force."""
+
+from itertools import product
 import numpy
+from scipy.special import erfc
+#This comes from nbodykit
+from pmesh.pm import ParticleMesh
 
 class NDiff:
-    D = {
+    """Long-range differentiation kernels."""
+    Defs = {
         #http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/smooth-low-noise-differentiators/
         'snrd2_5': (8, (2, 1)),
         'snrd2_7': (32, (5, 4, 1)),
         'snrd2_9': (128, (14, 14, 6, 1)),
-        'snrd2_11': (512, (42, 48, 27, 8, 1)), 
+        'snrd2_11': (512, (42, 48, 27, 8, 1)),
         'snrd4_7': (96, (39, 12, -5)),
         'snrd4_9': (96, (27, 16, -1, -2)),
         'snrd4_11': (1536, (322, 256, 39, -32, -11)),
         #http://www.holoborodko.com/pavel/numerical-methods/numerical-derivative/lanczos-low-noise-differentiators/
         'lnld2_5': (10, (1, 2)),
         'lnld2_7': (28, (1, 2, 3)),
-        'lnld2_7': (60, (1, 2, 3, 4)),
-        'lnld2_9': (110, (1, 2, 3, 4, 5)),
+        'lnld2_9': (60, (1, 2, 3, 4)),
+        'lnld2_11': (110, (1, 2, 3, 4, 5)),
         'lnld4_5': (12, (8, -1,)),    # default in MP-Gadget3
         'lnld4_7': (252, (58, 67, -22)),
         'lnld4_9': (1188, (126, 193,142, -86)),
@@ -29,46 +36,52 @@ class NDiff:
     }
     def __init__(self, name):
         self.name = name
-        self.norm = self.D[name][0]
-        self.coeffs = numpy.array(self.D[name][1], 'f8')
+        self.norm = self.Defs[name][0]
+        self.coeffs = numpy.array(self.Defs[name][1], 'f8')
 
         s = sum((i+1) * c for i, c in enumerate(self.coeffs))
         #print(self.name, self.norm, s)
         assert self.norm  == 2.0 * s
 
-    def __call__(self, w):
-        return 2 * 1.0 / self.norm * numpy.sum(numpy.sin((i+1) * w) * c for i, c in enumerate(self.coeffs))
+    def __call__(self, ww):
+        return 2 * 1.0 / self.norm * numpy.sum(numpy.sin((i+1) * ww) * c for i, c in enumerate(self.coeffs))
 
 def decic(k, v):
     """ Deconvolves the CIC window, once"""
     f = 1
-    for dir in range(3):
-        cellsize = (v.BoxSize[dir] / v.Nmesh[dir])
-        wd = k[dir] * cellsize
+    for axis in range(3):
+        cellsize = (v.BoxSize[axis] / v.Nmesh[axis])
+        wd = k[axis] * cellsize
         tmp = numpy.sinc(wd * 0.5 / numpy.pi)
         f = f * 1 / tmp ** 2
     return v * f
 
-# low noise differentiator
-def gradient(dir, dfkernel):
-    def kernel(k, v, df=dfkernel, dir=dir):
-        cellsize = (v.BoxSize[dir] / v.Nmesh[dir])
-        w = k[dir] * cellsize
+def gradient(axis, dfkernel):
+    """low noise differentiator"""
+    def kernel(k, v):
+        """Kernel function for long-range gravity gradient operator"""
+        cellsize = (v.BoxSize[axis] / v.Nmesh[axis])
+        w = k[axis] * cellsize
         a = dfkernel(w) / cellsize
         return v * (1j * a)
     return kernel
 
 def longrange(r_split):
+    """Smoothing kernel for the long range force."""
     if r_split != 0:
         def kernel(k, v):
+            """Kernel function for smoothing kernel"""
             kk = sum(ki ** 2 for ki in k)
             return v * numpy.exp(-kk * r_split**2)
     else:
         def kernel(k, v):
+            """Null smoothing kernel"""
+            _ = k
             return v
     return kernel
 
 def laplace(k, v):
+    """Multiplies the fourier grid by a Laplacian to compute force."""
     kk = sum(ki ** 2 for ki in k)
     mask = (kk == 0).nonzero()
     kk[mask] = 1
@@ -77,8 +90,15 @@ def laplace(k, v):
     return b
 
 def force_pm(pm, x, y, dfkernel=NDiff('lnld4_5'), split=1.25, compensate_cic=True):
-    """ split is in unit of mesh cells, 1.25 is Gadget Asmth.
-        lnld4_5 is gadget differentiation.
+    """
+    pm: PM grid to use.
+    x, y: vectors of particles for which positions are computed.
+    dfkernel: Force kernel to use. lnld4_5 is gadget differentiation.
+    split: in unit of mesh cells, 1.25 is Gadget Asmth.
+    Signifies there is a short-range force and thus the kernel should be smoothed.
+    Higher values reduce mesh anisotropy.
+    compensate_cic: Should we compensate for the window function from adding particles to the grid?
+    Gadget does.
     """
     rho = pm.paint(y)
     x = numpy.array(x)
@@ -107,9 +127,8 @@ def force_pm(pm, x, y, dfkernel=NDiff('lnld4_5'), split=1.25, compensate_cic=Tru
 
 # brute force with summation of images,
 
-from itertools import product
-
 def gravity_spline(dist, a):
+    """This computes the direct gravitational force for a particle softened with a spline, as is done in Gadget."""
     # copied from Gadget / check for typos? But we really only care the very outside
     r = numpy.einsum('...j,...j->...', dist, dist) ** 0.5
     fac = 1 / r ** 3
@@ -121,45 +140,52 @@ def gravity_spline(dist, a):
     mid   = (u >= 0.5) & (u < 1.0)
     fac[inner] = (a ** -3 * ( 32. / 3 + u * u * (32.0 * u - 38.4)))[inner]
     fac[mid] = (a ** -3 * ( 64. / 3 - 48.0 * u
-                        + 38.4 * u * u
-                        - 32.0 / 3 * u * u * u
-                        - 0.2/3 / (u * u * u)))[mid]
+                            + 38.4 * u * u
+                            - 32.0 / 3 * u * u * u
+                            - 0.2/3 / (u * u * u)))[mid]
 
-    pot[inner] = (a**-1 * (
-            -2.8 + u * u * (16./3 + u * u * (6.4 * u - 0.6))
-        ))[inner]
-    pot[mid]  = (a**-1 * (
-            -3.2 + 0.2 / 3 / u + u * u * (32. / 3 + u * (-16.0 + u *(9.6 - 6.4 / 3 * u)))
-        ))[mid]
+    pot[inner] = (a**-1 * (-2.8 + u * u * (16./3 + u * u * (6.4 * u - 0.6))))[inner]
+    pot[mid]  = (a**-1 * (-3.2 + 0.2 / 3 / u + u * u * (32. / 3 + u * (-16.0 + u *(9.6 - 6.4 / 3 * u)))))[mid]
 
     return dist * fac[..., None], -pot
 
 def gravity_plummer(dist, a):
+    """The computes the direct force for a point particle"""
     r2 = numpy.einsum('...j, ...j->...', dist, dist)
     r2 = r2 + a**2
 
-    return (1.0 / (r2 * r2 ** 0.5)[..., None] * dist,
-           1.0 / (r2 ** 0.5))
+    return (1.0 / (r2 * r2 ** 0.5)[..., None] * dist, 1.0 / (r2 ** 0.5))
 
-from scipy.special import erfc
 # the 'correct' kernel from gadget paper
-def w(u, split):
+def g2_force_kern(u, split):
+    """Gadget kernel for the force."""
     u = u * 0.5 / split
     u = abs(u)
     return erfc(u) + 2 * u / numpy.pi ** 0.5 * numpy.exp(-u * u)
-def v(u, split):
+
+def g2_pot_kern(u, split):
+    """Gadget kernel for the potential."""
     u = u * 0.5 / split
     u = abs(u)
     return erfc(u)
 
 
 def force_direct(pm, x, y, a=1/20., kernel=gravity_plummer, split=False, Nimg=4):
-    """ Returns
+    """
+    split: if true, force is split into long and short-range parts, as in Gadget, and the kernel is divided
+           by the analytic error function window function.
+    pm: only used if split=True. Used to compute the erf window in units of the PM grid.
+    x, y: vectors of particle coordinates
+    a: softening length in units of grid.
+    kernel: force kernel to use: either 1/r^2 or softened.
+    Nimg: ???
+    Returns
         f, p : force and potential
     """
     # fix me : unreasonably slow this is.
 
     def dochunk(x1):
+        """Evaluate the potential for a chunk of the total."""
         dist = []
 
         for ix, iy, iz in product(*([range(-Nimg, Nimg+1)] * 3)):
@@ -177,8 +203,8 @@ def force_direct(pm, x, y, a=1/20., kernel=gravity_plummer, split=False, Nimg=4)
         if split:
             r = (dist **2).sum(axis=-1) ** 0.5
             u = r / (pm.BoxSize[0] / pm.Nmesh[0])
-            fa = fa * w(u, split)[..., None]
-            pa = pa * v(u, split)
+            fa = fa * g2_force_kern(u, split)[..., None]
+            pa = pa * g2_pot_kern(u, split)
 
         f = (fa.sum(axis=0))
         p = (pa.sum(axis=0))
@@ -206,6 +232,7 @@ def force_direct(pm, x, y, a=1/20., kernel=gravity_plummer, split=False, Nimg=4)
     return F, P
 
 def main(ns):
+    """Main routine that makes the plots."""
     pm = ParticleMesh(BoxSize=512., Nmesh=[512, 512, 512], dtype='f4')
 
     Q = numpy.array([
@@ -236,36 +263,37 @@ def main(ns):
     # r is in mesh units
     r = ((test[:] - Q[0]) ** 2).sum(axis=-1) ** 0.5 / (pm.BoxSize[0] / pm.Nmesh[0])
 
-    def compute(test, Q):
-        f_longrange, p_longrange = force_pm(pm, test, Q, split=Split, compensate_cic=ns.decic)
-        f_plummer, p_plummer = force_direct(pm, test, Q, a=Smoothing, kernel=gravity_plummer)
-        f_spline, p_spline = force_direct(pm, test, Q, a=Smoothing, kernel=gravity_spline, Nimg=4)
-        f_erf, p_erf = force_direct(pm, test, Q, a=Smoothing, kernel=gravity_spline, split=Split, Nimg=0)
+    def compute_single(force_potential):
+        """Do computations for a force kernel option set."""
+        f, p = force_potential
+        f = numpy.einsum('ij,ij->i', f, unitvectors)
+        p[r == 0] = 0
+        f[r == 0] = 0
+        return f, p
 
-        f_longrange = numpy.einsum('ij,ij->i', f_longrange, unitvectors)
-        f_plummer = numpy.einsum('ij,ij->i', f_plummer, unitvectors)
-        f_spline = numpy.einsum('ij,ij->i', f_spline, unitvectors)
-        f_erf = numpy.einsum('ij,ij->i', f_erf, unitvectors)
+    def compute(test, Q):
+        """Do the computations for the various different types of force kernels."""
+        #This is the long-range PM force
+        f_longrange, p_longrange = compute_single(force_pm(pm, test, Q, split=Split, compensate_cic=ns.decic))
+        #This is the short-range force for a particle with softening but without long-range interactions at all.
+        f_spline, p_spline = compute_single(force_direct(pm, test, Q, a=Smoothing, kernel=gravity_spline, Nimg=4))
+        #This is the short-range force for a particle with softening and a long-range smoothing kernel applied.
+        f_erf, p_erf = compute_single(force_direct(pm, test, Q, a=Smoothing, kernel=gravity_spline, split=Split, Nimg=0))
+        #This is the short-range force for a particle without softening and no long-range force.
+        #Not actually used.
+        f_plummer, p_plummer = compute_single(force_direct(pm, test, Q, a=Smoothing, kernel=gravity_plummer))
 
         # renormalize
-        p_plummer -= p_plummer[-1] - p_longrange[-1]
         p_spline -= p_spline[-1] - p_longrange[-1]
+        p_plummer -= p_plummer[-1] - p_longrange[-1]
 
-        p_spline[r == 0] = 0
-        p_plummer[r == 0] = 0
-        p_longrange[r == 0] = 0
-        p_erf[r == 0] = 0
-
-        f_longrange[r == 0] = 0
-        f_plummer[r == 0] = 0
-        f_spline[r == 0] = 0
-        f_erf[r == 0] = 0
         return (f_longrange, f_plummer, f_spline, f_erf,
                 p_longrange, p_plummer, p_spline, p_erf, )
 
     terms = [[],] * 8
 
-    for junk in range(Nshift):
+    #Compute forces for some randomly positioned particles.
+    for _ in range(Nshift):
         shift = numpy.random.uniform(low=-0.5, high=0.5, size=(1, 3))
         for i, result in enumerate(compute(test + shift, Q + shift)):
             terms[i] = numpy.concatenate([terms[i], result], axis=0)
@@ -273,34 +301,35 @@ def main(ns):
     for i, result in enumerate(terms):
         terms[i] = terms[i].reshape(Nshift, -1, Nsample).transpose((1, 2, 0)).ravel()
 
-    (f_longrange, f_plummer, f_spline, f_erf,
-     p_longrange, p_plummer, p_spline, p_erf, ) = terms
+    (f_longrange, _, f_spline, f_erf,
+     p_longrange, _, p_spline, p_erf, ) = terms
     print(len(terms[0]))
 
     def stat(x, size):
+        """Compute mean and sd for different force kernels"""
         x = x.reshape(-1, size)
         mean = numpy.mean(x, axis=-1)
         std = numpy.std(x, axis=-1)
         return mean, std
 
-    rx, junk = stat(r, Nsample)
+    rx, _ = stat(r, Nsample)
+    #Variance between long-range force and direct force. Measures mesh anisotropy.
     rp_1d, rp_1d_s = stat((p_spline - p_longrange) / p_spline, Nsample * Nshift)
     rf_1d, rf_1d_s = stat((f_spline - f_longrange) / f_spline, Nsample * Nshift)
-    rp_erf, junk = stat((p_erf) / p_spline, Nsample * Nshift)
-    rf_erf, junk = stat((f_erf) / f_spline, Nsample * Nshift)
+    rp_erf, _ = stat((p_erf) / p_spline, Nsample * Nshift)
+    rf_erf, _ = stat((f_erf) / f_spline, Nsample * Nshift)
 
     rp_1d[rx==0] = 1
     rf_1d[rx==0] = 1
     rp_erf[rx==0] = 1
     rf_erf[rx==0] = 1
 
-
     from matplotlib.figure import Figure
     from matplotlib.backends.backend_agg import FigureCanvasAgg
 
     figure = Figure(figsize=(8, 8))
     figure.text(0.5, 0.91, "Split=%g" % Split, ha='center', va='center')
-    canvas = FigureCanvasAgg(figure)
+    _ = FigureCanvasAgg(figure)
     ax = figure.add_subplot(221)
     ax.set_title('potential')
     l, = ax.plot(rx, rp_1d, '-', label='actual')
@@ -316,19 +345,15 @@ def main(ns):
 
     ax = figure.add_subplot(223)
     ax.set_title('relative and zoom')
-    l, = ax.plot(rx, rp_1d, '-', label='actual, zero-crossing')
     ax.fill_between(rx, -3 * rp_1d_s,  3 * rp_1d_s, color=l.get_color(), alpha=0.4, label='3-sigma')
-    ax.plot(rx, rp_erf / rp_1d - 1, label='erf, spline', ls=':')
-    ax.plot(rx, rp_erf, '--', label='erf, zero-crossing', color='gray')
+    ax.plot(rx, rp_erf / rp_1d - 1, label='erf/actual ratio (spline)', ls=':')
     ax.set_ylim(-0.02, 0.02)
     ax.grid()
     ax.legend()
     ax = figure.add_subplot(224)
     ax.set_title('relative and zoom')
-    l, = ax.plot(rx, rf_1d, '-', label='actual, zero-crossing')
     ax.fill_between(rx,  - 3 * rf_1d_s,  3 * rf_1d_s, color=l.get_color(), alpha=0.4, label='3-sigma')
-    ax.plot(rx, rf_erf / rf_1d - 1, label='erf, spline', ls=':')
-    ax.plot(rx, rf_erf, '--', label='erf, zero-crossing', color='gray')
+    ax.plot(rx, rf_erf / rf_1d - 1, label='erf/actual ratio (spline)', ls=':')
     ax.set_ylim(-0.03, 0.03)
     ax.grid()
     ax.legend()
@@ -339,20 +364,19 @@ def main(ns):
     table = numpy.array([rx, rp_1d, rf_1d, rp_erf, rf_erf]).T
 
     numpy.savetxt('shortrange-force-kernels.txt',
-        table,
-        header='x(in mesh units) w_pot_1d(x) w_force_1d(x) [erfc + other terms] w_pot_erf(x) w_force_erf(x) split=%.2f' % Split
-    )
+                  table, header='x(in mesh units) w_pot_1d(x) w_force_1d(x) [erfc + other terms] w_pot_erf(x) w_force_erf(x) split=%.2f' % Split
+                 )
 
     # simply multiple these numbers to the unwindowed short-range force.
-    s = toc(table,
-            "shortrange_force_kernels",
-         header='x(in mesh units) w_pot_1d(x)  w_force_1d(x) [erfc + other terms] w_pot_erf(x) w_force_erf(x) split=%.2f' % Split
-        )
+    s = toc(table, "shortrange_force_kernels",
+            header='x(in mesh units) w_pot_1d(x)  w_force_1d(x) [erfc + other terms] w_pot_erf(x) w_force_erf(x) split=%.2f' % Split
+           )
 
     with open('shortrange-kernels.c', 'w') as ff:
         ff.write(s)
 
 def toc(array, arrayname, header):
+    """Function to save the tables to a carefully formatted file."""
     template = """
 # %(header)s
 const double %(name)s[][%(size)d] = {
@@ -360,7 +384,7 @@ const double %(name)s[][%(size)d] = {
 %(text)s
 
     };
-    
+
     """
     t = []
     for row in array:
@@ -372,12 +396,13 @@ const double %(name)s[][%(size)d] = {
         size=array.shape[1],
         text='\n'.join(t)
     )
-    
-import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument('split', type=float, help='Split of range, in mesh units')
-ap.add_argument('--no-decic', action='store_false', dest='decic', default=True, help='deconvolve cic window')
-ap.add_argument('prefix', default="")
-ns = ap.parse_args()
 
-main(ns)
+if __name__ == "__main__":
+    import argparse
+    ap = argparse.ArgumentParser()
+    ap.add_argument('split', type=float, help='Split of range, in mesh units')
+    ap.add_argument('--no-decic', action='store_false', dest='decic', default=True, help='deconvolve cic window')
+    ap.add_argument('prefix', default="", help="Directory in which to save figures.")
+    opts = ap.parse_args()
+
+    main(opts)
