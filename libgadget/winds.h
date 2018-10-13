f19c3a6a50924c0e80070421a66e11b3fb8ee5ee
Simeon Bird <sbird@ucr.edu>
2018-10-13 16:39:23 -0700

    Factor the wind model into a separate file, winds.h

---
+++ libgadget/winds.h
@@ -0,0 +1,17 @@
+#ifndef WINDS_H
+#define WINDS_H
+#ifdef SFR
+
+#include "treewalk.h"
+
+/*Evolve a wind particle, reducing its DelayTime*/
+void wind_evolve(int i);
+
+/*do the treewalk for the wind model*/
+void winds_and_feedback(int * NewStars, int NumNewStars);
+
+/*Make a particle a wind particle by changing DelayTime to a positive number*/
+int make_particle_wind(MyIDType ID, int i, double v, double vmean[3]);
+
+#endif
+#endif
