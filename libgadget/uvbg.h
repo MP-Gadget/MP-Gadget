801b11fc8aba1ff41648b801de2cfaf9ca640977
Simon Mutch <smutch@unimelb.edu.au>
2019-05-09 18:13:46 +1000

    Begin hacking in an FFT implementation

--- libgadget/uvbg.h
+++ libgadget/uvbg.h
@@ -1,1 +1,16 @@
+#include <fftw3.h>
+
+typedef struct UVBGgrids {
+    ptrdiff_t *slab_nix;
+    ptrdiff_t *slab_ix_start;
+    ptrdiff_t *slab_n_complex;
+
+    float *deltax;
+    fftwf_complex *deltax_filtered;
+    // float *uvphot;
+    // fftwf_complex *uvphot_filtered;
+    // float *xHI;
+    // float *J21;
+} UVBGgrids;
+
 void calculate_uvbg();
