8e41013e5b2b29b5c18287ae084cecceabe7caf3
Simeon Bird <sbird@ucr.edu>
2019-06-01 23:52:38 -0700

    Merge the final call of density_update in init into the main entropy initialization loop.

--- libgadget/init.c
+++ libgadget/init.c
@@ -181,6 +181,8 @@ static void
 setup_density_indep_entropy(ForceTree * Tree, double u_init, double a3)
 {
     int j;
+    int stop = 0;
+
     message(0, "Converting u -> entropy, with density split sph\n");
 
     /* This gives better convergence than initializing EgyWtDensity before Density is known*/
@@ -200,6 +202,8 @@ setup_density_indep_entropy(ForceTree * Tree, double u_init, double a3)
         }
         /* Update the EgyWtDensity*/
         density(0, All.DensityIndependentSphOn, Tree);
+        if(stop)
+            break;
 
         double maxdiff = 0;
         #pragma omp parallel for reduction(max: maxdiff)
@@ -211,8 +215,10 @@ setup_density_indep_entropy(ForceTree * Tree, double u_init, double a3)
 
         message(0, "iteration %d, max relative change in EgyWtDensity = %g \n", j, maxdiff);
 
+        /* If maxdiff is small, do one more iteration and stop*/
         if(maxdiff < 1e-3)
-            break;
+            stop = 1;
+
     }
     myfree(olddensity);
 }
@@ -303,23 +309,18 @@ setup_smoothinglengths(int RestartSnapNum, DomainDecomp * ddecomp)
 
         u_init /= molecular_weight;
 
+        /* snapshot already has EgyWtDensity; hope it is read in correctly.
+         * (need a test on this!) */
         if(All.DensityIndependentSphOn) {
             setup_density_indep_entropy(&Tree, u_init, a3);
         }
-
-        /*Initialize to initial energy*/
-        #pragma omp parallel for
-        for(i = 0; i < PartManager->NumPart; i++) {
-            if(P[i].Type == 0) {
-                SPHP(i).Entropy = GAMMA_MINUS1 * u_init / pow(SPH_EOMDensity(i)/a3 , GAMMA_MINUS1);
-            }
+        else {
+           /*Initialize to initial energy*/
+            #pragma omp parallel for
+            for(i = 0; i < SlotsManager->info[0].size; i++)
+                SphP[i].Entropy = GAMMA_MINUS1 * u_init / pow(SphP[i].Density / a3 , GAMMA_MINUS1);
         }
     }
-    /* snapshot already has EgyWtDensity; hope it is read in correctly.
-     * (need a test on this!) */
-    if(All.DensityIndependentSphOn)
-        density(0, 1, &Tree);
-
     slots_free_sph_scratch_data(SphP_scratch);
     force_tree_free(&Tree);
 }
