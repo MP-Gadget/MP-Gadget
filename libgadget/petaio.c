c40f2a4f8cd7aa80d1b19c6e530e4717f51183f8
Simeon Bird <sbird@ucr.edu>
2019-06-27 18:36:14 -0700

    Save the state of particle ionization and quasar time for black holes to the snapshots

--- libgadget/petaio.c
+++ libgadget/petaio.c
@@ -820,6 +820,8 @@ SIMPLE_PROPERTY_PI(BlackholeAccretionRate, Mdot, float, 1, struct bh_particle_da
 SIMPLE_PROPERTY_PI(BlackholeProgenitors, CountProgs, float, 1, struct bh_particle_data)
 SIMPLE_PROPERTY_PI(BlackholeMinPotPos, MinPotPos[0], double, 3, struct bh_particle_data)
 SIMPLE_PROPERTY_PI(BlackholeJumpToMinPot, JumpToMinPot, int, 1, struct bh_particle_data)
+SIMPLE_PROPERTY_PI(BlackholeQuasarTime, QuasarTime, float, 1, struct bh_particle_data)
+
 /*This is only used if FoF is enabled*/
 SIMPLE_GETTER(GTGroupID, GrNr, uint32_t, 1, struct particle_data)
 static void GTNeutralHydrogenFraction(int i, float * out, void * baseptr, void * smanptr) {
@@ -844,6 +846,15 @@ static void STInternalEnergy(int i, float * out, void * baseptr, void * smanptr)
     struct slot_info * info = &(((struct slots_manager_type *) smanptr)->info[0]);
     struct sph_particle_data * sl = (struct sph_particle_data *) info->ptr;
     sl[PI].Entropy  = GAMMA_MINUS1 * u / pow(SPH_EOMDensity(i) * All.cf.a3inv , GAMMA_MINUS1);
+}
+
+/* Can't use the macros because cannot take address of a bitfield*/
+static void GTHeIIIIonized(int i, unsigned char * out) {
+    *out = P[i].Ionized;
+}
+
+static void STHeIIIIonized(int i, unsigned char * out) {
+    P[i].Ionized = *out;
 }
 
 static int order_by_type(const void *a, const void *b)
@@ -898,6 +909,8 @@ void register_io_blocks(struct IOTable * IOTable) {
     /* Cooling */
     IO_REG(ElectronAbundance,       "f4", 1, 0, IOTable);
     IO_REG_WRONLY(NeutralHydrogenFraction, "f4", 1, 0, IOTable);
+    /* Marks whether a particle has been HeIII ionized yet*/
+    IO_REG_NONFATAL(HeIIIIonized, "u1", 1, 0, IOTable);
 
     /* SF */
     IO_REG_WRONLY(StarFormationRate, "f4", 1, 0, IOTable);
@@ -916,6 +929,7 @@ void register_io_blocks(struct IOTable * IOTable) {
     IO_REG(BlackholeProgenitors,   "i4", 1, 5, IOTable);
     IO_REG(BlackholeMinPotPos, "f8", 3, 5, IOTable);
     IO_REG(BlackholeJumpToMinPot,   "i4", 1, 5, IOTable);
+    IO_REG_NONFATAL(BlackholeQuasarTime,     "f4", 1, 5, IOTable);
 
     /* Smoothing lengths for black hole: this is a new addition*/
     IO_REG_NONFATAL(SmoothingLength,  "f4", 1, 5, IOTable);
