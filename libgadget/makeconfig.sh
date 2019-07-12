1d9c64929189224a8e224248562b224e8121e322
Simeon Bird <sbird@ucr.edu>
2019-07-12 12:09:58 -0700

    Move GADGET_TESTDATA_ROOT definition into the Makefile

--- libgadget/makeconfig.sh
+++ libgadget/makeconfig.sh
@@ -14,7 +14,6 @@ GIT=`git describe --always --dirty --abbrev=10`
 VERSION=${VERSION}_${GIT/-/_}
 fi
 echo '#define GADGET_VERSION "'${VERSION}'"'
-echo '#define GADGET_TESTDATA_ROOT "'$PWD'/.."'
 ) > $1
 
 
