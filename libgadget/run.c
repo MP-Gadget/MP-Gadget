2e62b4703c89741e9f7cb72e994efb5b997b06a8
Simeon Bird <sbird@ucr.edu>
2019-09-12 16:40:45 -0700

    Add a debug check that the random shift is the same on all processors

--- libgadget/run.c
+++ libgadget/run.c
@@ -178,6 +178,16 @@ void run(DomainDecomp * ddecomp)
                 new_random_shift[i] = rr - All.CurrentParticleOffset[i];
                 All.CurrentParticleOffset[i] = rr;
             }
+#ifdef DEBUG
+            /* Check explicitly that the vector is the same on all processors*/
+            double test_random_shift[3] = {0};
+            for (i = 0; i < 3; i++)
+                test_random_shift[i] = All.CurrentParticleOffset[i];
+            MPI_Bcast(test_random_shift, 3, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            for (i = 0; i < 3; i++)
+                if(test_random_shift[i] != All.CurrentParticleOffset[i])
+                    endrun(44, "Random shift %d is %g != %g on task 0!\n", i, test_random_shift[i], All.CurrentParticleOffset[i]);
+#endif
         }
         /* Sync positions of all particles */
         drift_all_particles(All.Ti_Current, new_random_shift);
