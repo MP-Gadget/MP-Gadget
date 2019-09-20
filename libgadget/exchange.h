0150b7da94fa8925987afe45017fe178a3126b1d
Simeon Bird <sbird@ucr.edu>
2019-09-19 20:31:08 -0700

    Use a local SlotsManager pointer in exchange instead of the global

--- libgadget/exchange.h
+++ libgadget/exchange.h
@@ -2,10 +2,11 @@
 #define __EXCHANGE_H
 
 #include "partmanager.h"
+#include "slotsmanager.h"
 
 typedef int (*ExchangeLayoutFunc) (int p, const void * userdata);
 
-int domain_exchange(ExchangeLayoutFunc, const void * layout_userdata, int do_gc, struct part_manager_type * PartManager, MPI_Comm Comm);
+int domain_exchange(ExchangeLayoutFunc, const void * layout_userdata, int do_gc, struct part_manager_type * PartManager, struct slots_manager_type * SlotsManager, MPI_Comm Comm);
 void domain_test_id_uniqueness(struct part_manager_type * PartManager);
 
 #endif
