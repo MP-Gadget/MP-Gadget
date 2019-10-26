8152964a965d1453b6f97e6a87ae3fb8cba56467
Simeon Bird <sbird@ucr.edu>
2019-10-26 12:12:46 -0700

    Remove duplicate code

--- libgadget/utils/mpsort.c
+++ libgadget/utils/mpsort.c
@@ -9,14 +9,7 @@
 #include <mpi.h>
 
 #include "mpsort.h"
-
-#include <stdio.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-
+#include "system.h"
 
 typedef int (*_compar_fn_t)(const void * r1, const void * r2, size_t rsize);
 typedef void (*_bisect_fn_t)(void * r, const void * r1, const void * r2, size_t rsize);
@@ -452,10 +445,6 @@ static void piter_accept(struct piter * pi, char * P,
 }
 
 static int _mpsort_mpi_options = 0;
-
-static int MPI_Alltoallv_smart(void *sendbuf, int *sendcnts, int *sdispls,
-        MPI_Datatype sendtype, void *recvbuf, int *recvcnts,
-        int *rdispls, MPI_Datatype recvtype, MPI_Comm comm);
 
 /* mpi version of radix sort; 
  *
@@ -1356,183 +1345,6 @@ _solve_for_layout_mpi (
     return 0;
 }
 
-
-
-/* The following two functions are taken from MP-Gadget. The hope 
- * is that when the exchange is sparse posting requests is
- * faster than Alltoall on some implementations. */
-
-static int MPI_Alltoallv_sparse(void *sendbuf, int *sendcnts, int *sdispls,
-        MPI_Datatype sendtype, void *recvbuf, int *recvcnts,
-        int *rdispls, MPI_Datatype recvtype, MPI_Comm comm);
-
-int MPI_Alltoallv_smart(void *sendbuf, int *sendcnts, int *sdispls,
-        MPI_Datatype sendtype, void *recvbuf, int *recvcnts,
-        int *rdispls, MPI_Datatype recvtype, MPI_Comm comm) 
-/* 
- * sdispls, recvcnts rdispls can be NULL,
- *
- * if recvbuf is NULL, returns total number of item required to hold the
- * data.
- * */
-{
-    int ThisTask;
-    int NTask;
-    MPI_Comm_rank(comm, &ThisTask);
-    MPI_Comm_size(comm, &NTask);
-    int i;
-    int nn = 0;
-    int *a_sdispls=NULL, *a_recvcnts=NULL, *a_rdispls=NULL;
-    for(i = 0; i < NTask; i ++) {
-        if(sendcnts[i] > 0) {
-            nn ++;
-        }
-    }
-    if(recvcnts == NULL) {
-        a_recvcnts = malloc(sizeof(int) * NTask);
-        recvcnts = a_recvcnts;
-        MPI_Alltoall(sendcnts, 1, MPI_INT,
-                     recvcnts, 1, MPI_INT, comm);
-    }
-    if(recvbuf == NULL) {
-        int totalrecv = 0;
-        for(i = 0; i < NTask; i ++) {
-            totalrecv += recvcnts[i];
-        }
-        if(a_recvcnts)
-            free(a_recvcnts);
-        return totalrecv;
-    }
-    if(sdispls == NULL) {
-        a_sdispls = malloc(sizeof(int) * NTask);
-        sdispls = a_sdispls;
-        sdispls[0] = 0;
-        for (i = 1; i < NTask; i++) {
-            sdispls[i] = sdispls[i - 1] + sendcnts[i - 1];
-        }
-    }
-    if(rdispls == NULL) {
-        a_rdispls = malloc(sizeof(int) * NTask);
-        rdispls = a_rdispls;
-        rdispls[0] = 0;
-        for (i = 1; i < NTask; i++) {
-            rdispls[i] = rdispls[i - 1] + recvcnts[i - 1];
-        }
-    }
-
-    int dense;
-
-    if(mpsort_mpi_has_options(MPSORT_DISABLE_SPARSE_ALLTOALLV)
-    ) {
-        dense = 1;
-    } else {
-        dense = nn > 128;
-        MPI_Allreduce(MPI_IN_PLACE, &dense, 1, MPI_INT, MPI_SUM, comm);
-    }
-
-    if(mpsort_mpi_has_options(MPSORT_REQUIRE_SPARSE_ALLTOALLV)) {
-        dense = 0;
-    }
-
-    int ret;
-    if(dense != 0) {
-        ret = MPI_Alltoallv(sendbuf, sendcnts, sdispls,
-                    sendtype, recvbuf, 
-                    recvcnts, rdispls, recvtype, comm);
-    } else {
-        ret = MPI_Alltoallv_sparse(sendbuf, sendcnts, sdispls,
-                    sendtype, recvbuf, 
-                    recvcnts, rdispls, recvtype, comm);
-    }
-
-    if(a_rdispls)
-        free(a_rdispls);
-    if(a_sdispls)
-        free(a_sdispls);
-    if(a_recvcnts)
-        free(a_recvcnts);
-    return ret;
-}
-
-static int MPI_Alltoallv_sparse(void *sendbuf, int *sendcnts, int *sdispls,
-        MPI_Datatype sendtype, void *recvbuf, int *recvcnts,
-        int *rdispls, MPI_Datatype recvtype, MPI_Comm comm) {
-
-    int ThisTask;
-    int NTask;
-    MPI_Comm_rank(comm, &ThisTask);
-    MPI_Comm_size(comm, &NTask);
-    int PTask;
-    int ngrp;
-
-    for(PTask = 0; NTask > (1 << PTask); PTask++);
-
-    ptrdiff_t lb;
-    ptrdiff_t send_elsize;
-    ptrdiff_t recv_elsize;
-
-    MPI_Type_get_extent(sendtype, &lb, &send_elsize);
-    MPI_Type_get_extent(recvtype, &lb, &recv_elsize);
-
-#ifndef NO_ISEND_IRECV_IN_DOMAIN
-    int n_requests;
-    MPI_Request *requests = malloc(NTask * 2 * sizeof(MPI_Request));
-    n_requests = 0;
-
-
-    for(ngrp = 0; ngrp < (1 << PTask); ngrp++)
-    {
-        int target = ThisTask ^ ngrp;
-
-        if(target >= NTask) continue;
-        if(recvcnts[target] == 0) continue;
-        MPI_Irecv(
-                ((char*) recvbuf) + recv_elsize * rdispls[target], 
-                recvcnts[target],
-                recvtype, target, 101934, comm, &requests[n_requests++]);
-    }
-
-    MPI_Barrier(comm);
-    /* not really necessary, but this will guarantee that all receives are
-       posted before the sends, which helps the stability of MPI on
-       bluegene, and perhaps some mpich1-clusters */
-    /* Note 08/2016: Even on modern hardware this barrier leads to a slight speedup.
-     * Probably because it allows the code to hit a fast path transfer.*/
-
-    for(ngrp = 0; ngrp < (1 << PTask); ngrp++)
-    {
-        int target = ThisTask ^ ngrp;
-        if(target >= NTask) continue;
-        if(sendcnts[target] == 0) continue;
-        MPI_Isend(((char*) sendbuf) + send_elsize * sdispls[target], 
-                sendcnts[target],
-                sendtype, target, 101934, comm, &requests[n_requests++]);
-    }
-
-    MPI_Waitall(n_requests, requests, MPI_STATUSES_IGNORE);
-    free(requests);
-#else
-    for(ngrp = 0; ngrp < (1 << PTask); ngrp++)
-    {
-        int target = ThisTask ^ ngrp;
-
-        if(target >= NTask) continue;
-        if(sendcnts[target] == 0 && recvcnts[target] == 0) continue;
-        MPI_Sendrecv(((char*)sendbuf) + send_elsize * sdispls[target], 
-                sendcnts[target], sendtype, 
-                target, 101934,
-                ((char*)recvbuf) + recv_elsize * rdispls[target],
-                recvcnts[target], recvtype, 
-                target, 101934, 
-                comm, MPI_STATUS_IGNORE);
-
-    }
-#endif
-    /* ensure the collective-ness */
-    MPI_Barrier(comm);
-
-    return 0;
-}
 
 static void _mpsort_mpi_parse_env()
 {
