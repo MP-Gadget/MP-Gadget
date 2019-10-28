be2550cdd26a3188b3a7bf1ca935db2f969604cc
Simeon Bird <sbird@ucr.edu>
2019-10-28 10:25:13 -0700

    Add brackets in mymalloc macro definitions

--- libgadget/utils/mymalloc.h
+++ libgadget/utils/mymalloc.h
@@ -18,12 +18,12 @@ void report_detailed_memory_usage(const char *label, const char * fmt, ...);
 #define  myrealloc(ptr, size)     allocator_realloc(A_MAIN, ptr, size)
 #define  myfree(x)                 allocator_free(x)
 
-#define  ma_malloc(name, type, nele)            (type*) allocator_alloc_bot(A_MAIN, name, sizeof(type) * nele)
-#define  ma_malloc2(name, type, nele)           (type*) allocator_alloc_top(A_MAIN, name, sizeof(type) * nele)
+#define  ma_malloc(name, type, nele)            (type*) allocator_alloc_bot(A_MAIN, name, sizeof(type) * (nele))
+#define  ma_malloc2(name, type, nele)           (type*) allocator_alloc_top(A_MAIN, name, sizeof(type) * (nele))
 #define  ma_free(p) allocator_free(p)
 
-#define  ta_malloc(name, type, nele)            (type*) allocator_alloc_bot(A_TEMP, name, sizeof(type) * nele)
-#define  ta_malloc2(name, type, nele)           (type*) allocator_alloc_top(A_TEMP, name, sizeof(type) * nele)
+#define  ta_malloc(name, type, nele)            (type*) allocator_alloc_bot(A_TEMP, name, sizeof(type) * (nele))
+#define  ta_malloc2(name, type, nele)           (type*) allocator_alloc_top(A_TEMP, name, sizeof(type) * (nele))
 #define  ta_reset()     allocator_reset(A_TEMP, 0)
 #define  ta_free(p) allocator_free(p)
 
