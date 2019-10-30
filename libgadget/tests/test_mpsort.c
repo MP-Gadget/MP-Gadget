bba83ab2ddbe8337f3eb3e39da325cd6003d1f80
Simeon Bird <sbird@ucr.edu>
2019-10-29 17:04:03 -0700

    Implement a test for the large radix in the FoF code.
    This fails for > 8 processors.

--- libgadget/tests/test_mpsort.c
+++ libgadget/tests/test_mpsort.c
@@ -17,6 +17,13 @@
 #include "../utils/mymalloc.h"
 #include "../utils/mpsort.h"
 
+struct BaseGroup {
+    int OriginalTask;
+    int Length;
+    uint64_t MinID;
+    int MinIDTask;
+};
+
 static void radix_int(const void * ptr, void * radix, void * arg) {
     *(uint64_t*)radix = *(const int64_t*) ptr + INT64_MIN;
 }
@@ -59,6 +66,9 @@ check_sorted(void * data, int elsize, size_t localsize, int compar(void * d1, vo
 
     for(i = 1; i < localsize; i ++) {
         if(compar(data + i*elsize, data + (i - 1)*elsize) < 0) {
+//             struct BaseGroup * i1 = ((struct BaseGroup *)data) + i;
+//             struct BaseGroup * i2= ((struct BaseGroup *)data) +(i-1);
+//             message(1, "cur=(%ld %ld %ld) prev= (%ld %ld %ld)\n", labs(i1->OriginalTask - i1->MinIDTask), i1->MinID, i1->Length, labs(i2->OriginalTask - i2->MinIDTask), i2->MinID, i2->Length);
             endrun(12, "Ordering of local array is broken i=%ld, d=%ld d-1=%ld. \n", i, ((int64_t *)data)[i], ((int64_t *)data)[i-1]);
         }
         assert_true(compar(data + i*elsize, data + (i - 1)*elsize) >=0);
@@ -192,6 +202,105 @@ do_mpsort_test(int64_t srcsize, int bits, int staggered, int gather)
     myfree(src);
 }
 
+static void fof_radix_Group_TotalCountTaskDiffMinID(const void * a, void * radix, void * arg) {
+    uint64_t * u = (uint64_t *) radix;
+    struct BaseGroup * f = (struct BaseGroup *) a;
+    u[0] = labs(f->OriginalTask - f->MinIDTask);
+    u[1] = f->MinID;
+    u[2] = UINT64_MAX - (f->Length);
+}
+
+int compar_bg(void * d1, void * d2)
+{
+    struct BaseGroup * i1 = d1;
+    struct BaseGroup * i2 = d2;
+    int dist1 = labs(i1->OriginalTask - i1->MinIDTask);
+    int dist2 = labs(i2->OriginalTask - i2->MinIDTask);
+    /* Note reversed sign! We want the largest groups first.*/
+    if(i1->Length < i2->Length)
+        return 1;
+    else if(i1->Length > i2->Length)
+        return -1;
+    if(i1->MinID < i2->MinID)
+        return -1;
+    else if(i1->MinID > i2->MinID)
+        return 1;
+    else if(dist1 < dist2)
+        return -1;
+    else if(dist1 > dist2)
+        return 1;
+    return 0;
+}
+
+static uint64_t
+checksum_minid(struct BaseGroup * data, size_t localsize, MPI_Comm comm)
+{
+    uint64_t sum = 0;
+    size_t i;
+    for(i = 0; i < localsize; i ++) {
+        sum += data[i].MinID;
+    }
+
+    MPI_Allreduce(MPI_IN_PLACE, &sum, 1, MPI_LONG, MPI_SUM, comm);
+    return sum;
+}
+
+/* Tests sorting with a very long radix, as is done in the FOF code*/
+static void
+do_long_radix_test(int srcsize)
+{
+    int ThisTask;
+    int NTask;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &NTask);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ThisTask);
+
+    struct BaseGroup * base = mymalloc("base", srcsize * sizeof(struct BaseGroup));
+
+    int i;
+    for(i = 0; i < srcsize; i++)
+    {
+        base[i].OriginalTask = ThisTask;	/* original task */
+        base[i].MinIDTask = random() % NTask;
+        base[i].MinID = random();
+        base[i].Length = 10;
+    }
+
+    int64_t srcsum = checksum_minid(base, srcsize, MPI_COMM_WORLD);
+    {
+        double start = MPI_Wtime();
+
+        mpsort_mpi_newarray(base, srcsize,
+                            base, srcsize,
+                            sizeof(struct BaseGroup),
+                            fof_radix_Group_TotalCountTaskDiffMinID, 24, NULL,
+                            MPI_COMM_WORLD);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+        double end = MPI_Wtime();
+
+        int64_t destsum = checksum_minid(base, srcsize, MPI_COMM_WORLD);
+
+        if(destsum != srcsum) {
+            endrun(5, "MPSort checksum is inconsistent.\n");
+            MPI_Abort(MPI_COMM_WORLD, -1);
+        }
+
+        check_sorted(base, sizeof(struct BaseGroup), srcsize, compar_bg, MPI_COMM_WORLD);
+
+        message(0, "MPSort total time: %g\n", end - start);
+    }
+    myfree(base);
+}
+
+static void
+test_basegroup(void ** state)
+{
+    do_long_radix_test(50);
+    /* This is chosen because it fails in travis*/
+    do_long_radix_test(0);
+}
+
 static void
 test_mpsort_bits(void ** state)
 {
@@ -227,6 +336,7 @@ int main(void)
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_mpsort_bits),
         cmocka_unit_test(test_mpsort_stagger),
+        cmocka_unit_test(test_basegroup),
         cmocka_unit_test(test_mpsort_gather),
     };
     return cmocka_run_group_tests_mpi(tests, NULL, NULL);
