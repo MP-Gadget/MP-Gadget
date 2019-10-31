78b148e1a97345723ad08f870eb2f507a71d2bbb
Simeon Bird <sbird@ucr.edu>
2019-10-30 18:26:31 -0700

    Rename part structs in exchange

--- libgadget/exchange.h
+++ libgadget/exchange.h
@@ -6,7 +6,7 @@
 
 typedef int (*ExchangeLayoutFunc) (int p, const void * userdata);
 
-int domain_exchange(ExchangeLayoutFunc, const void * layout_userdata, int do_gc, struct part_manager_type * PartManager, struct slots_manager_type * SlotsManager, MPI_Comm Comm);
-void domain_test_id_uniqueness(struct part_manager_type * PartManager);
+int domain_exchange(ExchangeLayoutFunc, const void * layout_userdata, int do_gc, struct part_manager_type * pman, struct slots_manager_type * sman, MPI_Comm Comm);
+void domain_test_id_uniqueness(struct part_manager_type * pman);
 
 #endif
