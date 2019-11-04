305e5f073435d5eda03502428597aa85edc50095
Yu Feng <rainwoodman@gmail.com>
2019-11-03 22:45:34 -0800

    update hydro run to 12 ranks.

--- examples/hydro/run.sh
+++ examples/hydro/run.sh
@@ -1,4 +1,4 @@
-export OMP_NUM_THREADS=2
+export OMP_NUM_THREADS=1
 ROOT=../../
-mpirun -np 4 $ROOT/genic/MP-GenIC paramfile.genic || exit 1
-mpirun -np 4 $ROOT/gadget/MP-Gadget paramfile.gadget || exit 1
+mpirun -np 12 $ROOT/genic/MP-GenIC paramfile.genic || exit 1
+mpirun -np 12 $ROOT/gadget/MP-Gadget paramfile.gadget || exit 1
