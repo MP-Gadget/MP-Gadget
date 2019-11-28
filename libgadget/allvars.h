b6e9816f3c14f31b9932dc1b5fef370a87d9cf5b
Simeon Bird <sbird@ucr.edu>
2019-11-28 01:24:34 -0800

    Remove obsolete header

--- libgadget/allvars.h
+++ libgadget/allvars.h
@@ -1,31 +1,16 @@
 /*! \file allvars.h
- *  \brief declares global variables.
+ *  \brief declares the All structure.
  *
- *  This file declares all global variables. Further variables should be added here, and declared as
- *  'extern'. The actual existence of these variables is provided by the file 'allvars.c'. To produce
- *  'allvars.c' from 'allvars.h', do the following:
- *
- *     - Erase all #define statements
- *     - add #include "allvars.h"
- *     - delete all keywords 'extern'
- *     - delete all struct definitions enclosed in {...}, e.g.
- *        "extern struct global_data_all_processes {....} All;"
- *        becomes "struct global_data_all_processes All;"
  */
 
 #ifndef ALLVARS_H
 #define ALLVARS_H
 
 #include <mpi.h>
-#include <stdio.h>
-
 #include <omp.h>
-#include <pthread.h>
 
 #include "cosmology.h"
 #include "gravity.h"
-
-#include "assert.h"
 #include "physconst.h"
 #include "types.h"
 
