ef891789a47bc01bec859e082492db0385d805ae
Simeon Bird <sbird@ucr.edu>
2020-01-22 16:33:47 -0800

    Change the default TimeBetweenSeedingSearch to a more reasonable value

--- gadget/params.c
+++ gadget/params.c
@@ -246,7 +246,7 @@ create_gadget_parameter_set()
     param_declare_double(ps, "FOFHaloLinkingLength", OPTIONAL, 0.2, "Linking length for Friends of Friends halos.");
     param_declare_int(ps, "FOFHaloMinLength", OPTIONAL, 32, "Minimum number of particles per FOF Halo.");
     param_declare_double(ps, "MinFoFMassForNewSeed", OPTIONAL, 5e2, "Minimal sized halo (in number of DM particles) into which we should seed tracer particles.");
-    param_declare_double(ps, "TimeBetweenSeedingSearch", OPTIONAL, 1e5, "Time Between Seeding Attempts: default to a a large value, meaning never.");
+    param_declare_double(ps, "TimeBetweenSeedingSearch", OPTIONAL, 1.04, "Scale factor fraction increase between Seeding Attempts.");
 
     /*Black holes*/
     param_declare_int(ps, "BlackHoleOn", REQUIRED, 1, "Master switch to enable black hole formation and feedback. If this is on, type 5 particles are treated as black holes.");
