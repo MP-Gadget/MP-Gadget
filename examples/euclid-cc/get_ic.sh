3acd252a810fb40276a1656992b4209ea5349a24
Simeon Bird <sbird@ucr.edu>
2020-07-23 15:30:12 -0700

    Specify Mpc units

--- examples/euclid-cc/get_ic.sh
+++ examples/euclid-cc/get_ic.sh
@@ -2,4 +2,4 @@ echo "Downloading ICs:"
 wget -r -l1 --no-parent -nd -P euclidIC_2048_G1 "https://www.ics.uzh.ch/~aurel/euclidIC_2048/"
 
 echo "Converting to BigFile:"
-python ../../tools/convert_from_gadget_1.py "euclidIC_2048_G1/Planck2013-Npart_2048_Box_500-Fiducial" "euclidIC_2048"
+python ../../tools/convert_from_gadget_1.py "euclidIC_2048_G1/Planck2013-Npart_2048_Box_500-Fiducial" "euclidIC_2048" --unit-system Mpc
