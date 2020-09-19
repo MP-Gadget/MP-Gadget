89f7b0687230b39f5bfa68d7df9dddf003da0df0
Simeon Bird <sbird@ucr.edu>
2020-09-18 21:15:49 -0700

    Remove dead code for unused drift table

--- libgadget/timefac.h
+++ libgadget/timefac.h
@@ -8,13 +8,7 @@ void init_drift_table(Cosmology * CP, double timeBegin, double timeMax);
 double get_hydrokick_factor(inttime_t ti0, inttime_t ti1);
 double get_gravkick_factor(inttime_t ti0, inttime_t ti1);
 
-/* Get the drift factor at given time from a cache.
- * Run is terminated if
- * ti0 and ti1 are not in the cache*/
-double get_drift_factor(inttime_t ti0, inttime_t ti1);
-
-/* Get the exact drift factor at given time by integrating.
- */
+/* Get the exact drift and kick factors at given time by integrating. */
 double get_exact_drift_factor(Cosmology * CP, inttime_t ti0, inttime_t ti1);
 
 #endif
