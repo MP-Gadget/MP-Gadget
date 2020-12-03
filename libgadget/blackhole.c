2bf1bb0f98d0f0c84117345c49d22871b7fe3138
yueyingn <52514409+yueyingn@users.noreply.github.com>
2020-12-03 18:07:35 -0500

    fix a bug in BH_DRAG
    `fac*= a`  should be outside the loop.

--- libgadget/blackhole.c
+++ libgadget/blackhole.c
@@ -865,8 +865,8 @@ blackhole_accretion_postprocess(int i, TreeWalk * tw)
         double fac = 0;
         if (blackhole_params.BH_DRAG == 1) fac = BHP(i).Mdot/P[i].Mass;
         if (blackhole_params.BH_DRAG == 2) fac = blackhole_params.BlackHoleEddingtonFactor * meddington/BHP(i).Mass;
+        fac *= All.cf.a; /* dv = acc * kick_fac = acc * a^{-1}dt, therefore acc = a*dv/dt  */
         for(k = 0; k < 3; k++) {
-            fac *= All.cf.a; /* dv = acc * kick_fac = acc * a^{-1}dt, therefore acc = a*dv/dt  */
             BHP(i).DragAccel[k] = -(P[i].Vel[k] - BH_GET_PRIV(tw)->BH_SurroundingGasVel[PI][k])*fac;
         }
     }
