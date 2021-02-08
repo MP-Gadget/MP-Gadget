910d0d329b550b7ec4989ebaeb88957ef928b534
daviesje <daviesje@student.unimelb.edu.au>
2021-02-09 10:26:41 +1100

    restore examples defaults

--- examples/small/run.sh
+++ examples/small/run.sh
@@ -1,4 +1,4 @@
 export OMP_NUM_THREADS=2
 ROOT=../../
-mpirun -np 1 $ROOT/genic/MP-GenIC paramfile.genic || exit 1
-mpirun -np 1 $ROOT/gadget/MP-Gadget paramfile.gadget || exit 1
+mpirun -np 4 $ROOT/genic/MP-GenIC paramfile.genic || exit 1
+mpirun -np 4 $ROOT/gadget/MP-Gadget paramfile.gadget || exit 1
