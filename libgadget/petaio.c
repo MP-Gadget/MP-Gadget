8a08736897ee8b4bc417001a2a16674c41fb2429
daviesje <daviesje@student.unimelb.edu.au>
2021-02-09 12:40:24 +1100

    output local particle J21

--- libgadget/petaio.c
+++ libgadget/petaio.c
@@ -860,6 +860,10 @@ SIMPLE_PROPERTY_PI(BlackholeJumpToMinPot, JumpToMinPot, int, 1, struct bh_partic
 SIMPLE_PROPERTY_PI(BlackholeMtrack, Mtrack, float, 1, struct bh_particle_data)    
     
 SIMPLE_SETTER_PI(STBlackholeMinPotPos , MinPotPos[0], double, 3, struct bh_particle_data)
+
+/* extra properties from excursion set addition */
+SIMPLE_GETTER_PI(GTJ21, local_J21, float, 1, struct sph_particle_data)
+    
 static void GTBlackholeMinPotPos(int i, double * out, void * baseptr, void * smanptr) {
     /* Remove the particle offset before saving*/
     struct particle_data * part = (struct particle_data *) baseptr;
@@ -1034,6 +1038,10 @@ void register_io_blocks(struct IOTable * IOTable, int WriteGroupID) {
     IO_REG_NONFATAL(BlackholeSwallowID, "u8", 1, 5, IOTable);
     /* Time the BH was swallowed*/
     IO_REG_NONFATAL(BlackholeSwallowTime, "f4", 1, 5, IOTable);
+
+    /* excursion set */
+    IO_REG_WRONLY(J21,"f4",1,0,IOTable);
+    /* end excursion set*/
 
     /*Sort IO blocks so similar types are together; then ordered by the sequence they are declared. */
     qsort_openmp(IOTable->ent, IOTable->used, sizeof(struct IOTableEntry), order_by_type);
