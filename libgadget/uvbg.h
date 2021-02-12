2b88258018b8cb1e70a7214d969aebc054792e84
daviesje <daviesje@student.unimelb.edu.au>
2021-02-12 16:08:13 +1100

    fix partial ionisation bug, WIP grid saving

--- libgadget/uvbg.h
+++ libgadget/uvbg.h
@@ -23,7 +23,7 @@ extern struct UVBGgrids_type UVBGgrids;
 int grid_index(int i, int j, int k, ptrdiff_t strides[3]);
 double time_to_present(double a);
 void calculate_uvbg();
-void save_uvbg_grids(int SnapshotFileCount);
+void save_uvbg_grids(int SnapshotFileCount, PetaPM * pm);
 void set_uvbg_params(ParameterSet * ps);
 
 #endif
