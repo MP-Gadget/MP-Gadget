2af53e66777b206c7e03e83a7523c02eb515c493
daviesje <daviesje@student.unimelb.edu.au>
2021-02-23 15:20:52 +1100

    update es_test example

--- examples/es_test/run.sh
+++ examples/es_test/run.sh
@@ -1,6 +1,6 @@
-export OMP_NUM_THREADS=$4
+export OMP_NUM_THREADS=2
 
 ROOT=../../
 
-mpirun -np 2 $ROOT/genic/MP-GenIC paramfile.genic && \
-mpirun -np 2 $ROOT/gadget/MP-Gadget paramfile.gadget 
+mpirun -np 4 $ROOT/genic/MP-GenIC paramfile.genic && \
+mpirun -np 4 $ROOT/gadget/MP-Gadget paramfile.gadget 
