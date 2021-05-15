1310f8cfd8733d8aba5af882d146861cbadbb406
Simeon Bird <sbird@ucr.edu>
2021-05-14 17:05:11 -0700

    Fix comment

--- gadget/params.c
+++ gadget/params.c
@@ -283,7 +283,7 @@ create_gadget_parameter_set()
     param_declare_double(ps, "WindSigma0", OPTIONAL, 353, "Reference halo circular velocity at which to evaluate wind speed. Needs ofjt10 wind model.");
     param_declare_double(ps, "WindSpeedFactor", OPTIONAL, 3.7, "Factor connecting wind speed to halo circular velocity. ofjt10 wind model.");
 
-    param_declare_double(ps, "WindFreeTravelLength", OPTIONAL, 20, "Expected decoupling distance for the wind in internal distance units. Default no decoupling as this produces a better gsmf.");
+    param_declare_double(ps, "WindFreeTravelLength", OPTIONAL, 20, "Expected decoupling distance for the wind in internal distance units.");
     param_declare_double(ps, "WindFreeTravelDensFac", OPTIONAL, 0.1, "If the density of the wind particle drops below this factor of the star formation density threshold, the gas will recouple.");
     param_declare_double(ps, "MinWindVelocity", OPTIONAL, 0, "Minimum velocity of the kicked particle in the wind, in internal units (physical km/s).");
     param_declare_double(ps, "MaxWindFreeTravelTime", OPTIONAL, 30, "Maximum time in Myrs for the wind to be decoupled.");
