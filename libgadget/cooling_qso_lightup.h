08f1096d8170a11f90741c42fca7976724998e56
Simeon Bird <sbird@ucr.edu>
2021-10-06 17:00:35 -0700

    Save positions of the helium ionizing quasar to a file

--- libgadget/cooling_qso_lightup.h
+++ libgadget/cooling_qso_lightup.h
@@ -11,13 +11,16 @@ void set_qso_lightup_params(ParameterSet * ps);
 void init_qso_lightup(char * reion_hist_file);
 
 /* Starts reionization by selecting the first halo and flagging all particles in the first HeIII bubble*/
-void do_heiii_reionization(double redshift, FOFGroups * fof, ForceTree * tree);
+void do_heiii_reionization(double redshift, FOFGroups * fof, ForceTree * tree, FILE * FdHelium);
 
 /* Get the long mean free path photon heating that applies to not-yet-ionized particles*/
 double get_long_mean_free_path_heating(double redshift);
 
 /* Returns 1 if helium reionization is in progress, 0 otherwise*/
 int during_helium_reionization(double redshift);
+
+/* Returns 1 if the helium model is enabled*/
+int qso_lightup_on(void);
 
 /* Returns 1 if there is any work to do to ionize more particles, 0 otherwise*/
 int need_change_helium_ionization_fraction(double atime);
