08141d6f8494abd746383d1994be4acc67000fd5
Simeon Bird <sbird@ucr.edu>
2021-12-30 08:25:36 -0800

    Do not build the gravity tree if no gravitationally active particles
    This should avoid computing moments for short timesteps. We still need a
    full DM tree for the BH and wind codes, but we also try to defer
    building them if we can.

--- libgadget/winds.h
+++ libgadget/winds.h
@@ -28,13 +28,13 @@ void init_winds(double FactorSN, double EgySpecSN, double PhysDensThresh, double
 void winds_evolve(int i, double a3inv, double hubble);
 
 /*do the treewalk for the wind model*/
-void winds_and_feedback(int * NewStars, int NumNewStars, const double Time, const double hubble, ForceTree * tree);
+void winds_and_feedback(int * NewStars, int NumNewStars, const double Time, const double hubble, ForceTree * tree, DomainDecomp * ddecomp);
 
 /*Make a wind particle at the site of recent star formation.*/
 int winds_make_after_sf(int i, double sm, double vdisp, double atime);
 
 /* Make winds for the subgrid model, after computing the velocity dispersion. */
-void winds_subgrid(int * MaybeWind, int NumMaybeWind, const double Time, const double hubble, ForceTree * tree, MyFloat * StellarMasses);
+void winds_subgrid(int * MaybeWind, int NumMaybeWind, const double Time, const double hubble, ForceTree * tree, DomainDecomp * ddecomp, MyFloat * StellarMasses);
 
 /* Tests whether winds spawn from gas or stars*/
 int winds_are_subgrid(void);
