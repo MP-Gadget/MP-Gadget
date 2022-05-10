d6442480a85e2d88dd1cd261de1b484a2f941933
Simeon Bird <sbird@ucr.edu>
2022-05-10 10:41:39 -0700

    Add infrastructure to save short-range acceleration separately
    ie, not in GravAccel.

--- libgadget/gravshort.h
+++ libgadget/gravshort.h
@@ -51,6 +51,11 @@ struct GravShortPriv {
      * Note: should account for
      * massive neutrinos, but doesn't. */
     double cbrtrho0;
+    /* Whether to calculate the gravitational potential.
+     * Note this is only useful on steps where all particles are active*/
+    int CalcPotential;
+    /* (Optional) pointer to the place to store accelerations, if it is not P->GravAccel*/
+    MyFloat (*Accel)[3];
 };
 
 #define GRAV_GET_PRIV(tw) ((struct GravShortPriv *) ((tw)->priv))
@@ -59,16 +64,21 @@ static void
 grav_short_postprocess(int i, TreeWalk * tw)
 {
     double G = GRAV_GET_PRIV(tw)->G;
-    P[i].GravAccel[0] *= G;
-    P[i].GravAccel[1] *= G;
-    P[i].GravAccel[2] *= G;
+    MyFloat *GravAccel = NULL;
+    if(GRAV_GET_PRIV(tw)->Accel)
+        GravAccel = GRAV_GET_PRIV(tw)->Accel[i];
+    else
+        GravAccel = P[i].GravAccel;
+    GravAccel[0] *= G;
+    GravAccel[1] *= G;
+    GravAccel[2] *= G;
     /* calculate the potential */
     /* remove self-potential */
-    P[i].Potential += P[i].Mass / (FORCE_SOFTENING(i, P[i].Type) / 2.8);
-
-    P[i].Potential -= 2.8372975 * pow(P[i].Mass, 2.0 / 3) * GRAV_GET_PRIV(tw)->cbrtrho0;
-
-    P[i].Potential *= G;
+    if(GRAV_GET_PRIV(tw)->CalcPotential) {
+        P[i].Potential += P[i].Mass / (FORCE_SOFTENING(i, P[i].Type) / 2.8);
+        P[i].Potential -= 2.8372975 * pow(P[i].Mass, 2.0 / 3) * GRAV_GET_PRIV(tw)->cbrtrho0;
+        P[i].Potential *= G;
+    }
 }
 
 static void
@@ -80,11 +90,16 @@ grav_short_copy(int place, TreeWalkQueryGravShort * input, TreeWalk * tw)
 static void
 grav_short_reduce(int place, TreeWalkResultGravShort * result, enum TreeWalkReduceMode mode, TreeWalk * tw)
 {
+    MyFloat * GravAccel = NULL;
+    if(GRAV_GET_PRIV(tw)->Accel)
+        GravAccel = GRAV_GET_PRIV(tw)->Accel[place];
+    else
+        GravAccel = P[place].GravAccel;
     int k;
     for(k = 0; k < 3; k++)
-        TREEWALK_REDUCE(P[place].GravAccel[k], result->Acc[k]);
-
-    TREEWALK_REDUCE(P[place].Potential, result->Potential);
+        TREEWALK_REDUCE(GravAccel[k], result->Acc[k]);
+    if(GRAV_GET_PRIV(tw)->CalcPotential)
+        TREEWALK_REDUCE(P[place].Potential, result->Potential);
 }
 
 #endif
