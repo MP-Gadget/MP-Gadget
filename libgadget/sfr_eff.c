MP-Gadget/MP-Gadget

commit 1b397cc9c616f1d080e0a9d3b864fb2c232d0172

Adjust memory allocation for SPH_GradRho so it is lower after density()

libgadget/sfr_eff.c
@@ -124,7 +124,7 @@ static int make_particle_star(int child, int parent, int placement, double Time)
 static int starformation(int i, double *localsfr, MyFloat * sm_out, MyFloat * GradRho, const double redshift, const double a3inv, const double hubble, const double GravInternal, const struct UVBG * const GlobalUVBG);
 static int quicklyastarformation(int i, const double a3inv);
 static double get_sfr_factor_due_to_selfgravity(int i, const double atime, const double a3inv, const double hubble, const double GravInternal);
-static double get_sfr_factor_due_to_h2(int i, MyFloat * GradRho, const double atime);
+static double get_sfr_factor_due_to_h2(int i, MyFloat * GradRho_mag, const double atime);
 static double get_starformation_rate_full(int i, MyFloat * GradRho, struct sfr_eeqos_data sfr_data, const double atime, const double a3inv, const double hubble, const double GravInternal);
 static double get_egyeff(double redshift, double dens, struct UVBG * uvbg);
 static double find_star_mass(int i, const double avg_baryon_mass);
@@ -972,14 +972,14 @@ static double ev_NH_from_GradRho(MyFloat gradrho_mag, double hsml, double rho, d
     return ev_NH; // *(Z/Zsolar) add metallicity dependence
 }
 
-static double get_sfr_factor_due_to_h2(int i, MyFloat * GradRho, const double atime) {
+static double get_sfr_factor_due_to_h2(int i, MyFloat * GradRho_mag, const double atime) {
     /*  Krumholz & Gnedin fitting function for f_H2 as a function of local
      *  properties, from gadget-p; we return the enhancement on SFR in this
      *  function */
     double tau_fmol;
     const double a2 = atime * atime;
     double zoverzsun = SPHP(i).Metallicity/METAL_YIELD;
-    double gradrho_mag = sqrt(GradRho[3*P[i].PI]*GradRho[3*P[i].PI]+GradRho[3*P[i].PI+1]*GradRho[3*P[i].PI+1]+GradRho[3*P[i].PI+2]*GradRho[3*P[i].PI+2]);
+    double gradrho_mag = GradRho_mag[3*P[i].PI];
     //message(4, "GradRho %g rho %g hsml %g i %d\n", gradrho_mag, SPHP(i).Density, P[i].Hsml, i);
     tau_fmol = ev_NH_from_GradRho(gradrho_mag,P[i].Hsml,SPHP(i).Density,1) /a2;
     tau_fmol *= (0.1 + zoverzsun);

libgadget/density.c
@@ -213,7 +213,7 @@ static void density_copy(int place, TreeWalkQueryDensity * I, TreeWalk * tw);
  * neighbours.)
  */
 void
-density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int BlackHoleOn, double MinEgySpec, const DriftKickTimes times, Cosmology * CP, struct sph_pred_data * SPH_predicted, MyFloat * GradRho, const ForceTree * const tree)
+density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int BlackHoleOn, double MinEgySpec, const DriftKickTimes times, Cosmology * CP, struct sph_pred_data * SPH_predicted, MyFloat * GradRho_mag, const ForceTree * const tree)
 {
     TreeWalk tw[1] = {{0}};
     struct DensityPriv priv[1];
@@ -255,7 +255,10 @@ density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int Blac
 
     DENSITY_GET_PRIV(tw)->BlackHoleOn = BlackHoleOn;
     DENSITY_GET_PRIV(tw)->SPH_predicted = SPH_predicted;
-    DENSITY_GET_PRIV(tw)->GradRho = GradRho;
+    if(GradRho_mag)
+        DENSITY_GET_PRIV(tw)->GradRho = (MyFloat *) mymalloc("SPH_GradRho", sizeof(MyFloat) * 3 * SlotsManager->info[0].size);
+    else
+        DENSITY_GET_PRIV(tw)->GradRho = NULL;
 
     /* Init Left and Right: this has to be done before treewalk */
     #pragma omp parallel for
@@ -304,6 +307,17 @@ density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int Blac
     /* Do the treewalk with looping for hsml*/
     treewalk_do_hsml_loop(tw, act->ActiveParticle, act->NumActiveParticle, update_hsml);
 
+    if(GradRho_mag) {
+        #pragma omp parallel for
+        for(i = 0; i < SlotsManager->info[0].size; i++)
+        {
+            MyFloat * gr = DENSITY_GET_PRIV(tw)->GradRho + (3*i);
+            GradRho_mag[i] = sqrt(gr[0]*gr[0] + gr[1] * gr[1] + gr[2] * gr[2]);
+        }
+    }
+
+    if(DENSITY_GET_PRIV(tw)->GradRho)
+        myfree(DENSITY_GET_PRIV(tw)->GradRho);
     myfree(DENSITY_GET_PRIV(tw)->DhsmlDensityFactor);
     myfree(DENSITY_GET_PRIV(tw)->Rot);
     myfree(DENSITY_GET_PRIV(tw)->NumNgb);

libgadget/density.h
@@ -45,7 +45,7 @@ void set_densitypar(struct density_params dp);
  * it just computes densities.
  * If DoEgyDensity is true it also computes the entropy-weighted density for
  * pressure-entropy SPH. */
-void density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int BlackHoleOn, double MinEgySpec, const DriftKickTimes times, Cosmology * CP, struct sph_pred_data * SPH_predicted, MyFloat * GradRho, const ForceTree * const tree);
+void density(const ActiveParticles * act, int update_hsml, int DoEgyDensity, int BlackHoleOn, double MinEgySpec, const DriftKickTimes times, Cosmology * CP, struct sph_pred_data * SPH_predicted, MyFloat * GradRho_mag, const ForceTree * const tree);
 
 /* Get the desired nuber of neighbours for the supplied kernel*/
 double GetNumNgb(enum DensityKernelType KernelType);

libgadget/run.c
@@ -425,9 +425,9 @@ run(const int RestartSnapNum, const inttime_t ti_init, const struct header_data 
          * If so we need to add them to the tree.*/
         int HybridNuTracer = hybrid_nu_tracer(&All.CP, atime);
 
-        MyFloat * GradRho = NULL;
+        MyFloat * GradRho_mag = NULL;
         if(sfr_need_to_compute_sph_grad_rho())
-            GradRho = (MyFloat *) mymalloc2("SPH_GradRho", sizeof(MyFloat) * 3 * SlotsManager->info[0].size);
+            GradRho_mag = (MyFloat *) mymalloc2("SPH_GradRho", sizeof(MyFloat) * SlotsManager->info[0].size);
 
         /* density() happens before gravity because it also initializes the predicted variables.
         * This ensures that prediction consistently uses the grav and hydro accel from the
@@ -447,7 +447,7 @@ run(const int RestartSnapNum, const inttime_t ti_init, const struct header_data 
             struct sph_pred_data sph_predicted = slots_allocate_sph_pred_data(SlotsManager->info[0].size);
 
             if(All.DensityOn)
-                density(&Act, 1, DensityIndependentSphOn(), All.BlackHoleOn, MinEgySpec, times, &All.CP, &sph_predicted, GradRho, &gasTree);  /* computes density, and pressure */
+                density(&Act, 1, DensityIndependentSphOn(), All.BlackHoleOn, MinEgySpec, times, &All.CP, &sph_predicted, GradRho_mag, &gasTree);  /* computes density, and pressure */
 
             /***** update smoothing lengths in tree *****/
             force_update_hmax(Act.ActiveParticle, Act.NumActiveParticle, &gasTree, ddecomp);
@@ -630,7 +630,7 @@ run(const int RestartSnapNum, const inttime_t ti_init, const struct header_data 
 
             /**** radiative cooling and star formation *****/
             if(All.CoolingOn)
-                cooling_and_starformation(&Act, atime, &times, get_dloga_for_bin(times.mintimebin, times.Ti_Current), &Tree, GravAccel, ddecomp, &All.CP, GradRho, fds.FdSfr);
+                cooling_and_starformation(&Act, atime, &times, get_dloga_for_bin(times.mintimebin, times.Ti_Current), &Tree, GravAccel, ddecomp, &All.CP, GradRho_mag, fds.FdSfr);
         }
         /* We don't need this timestep's tree anymore.*/
         force_tree_free(&Tree);
@@ -715,9 +715,9 @@ run(const int RestartSnapNum, const inttime_t ti_init, const struct header_data 
         }
 
         /* Delayed here because it is allocated high before GravAccel*/
-        if(GradRho) {
-            myfree(GradRho);
-            GradRho = NULL;
+        if(GradRho_mag) {
+            myfree(GradRho_mag);
+            GradRho_mag = NULL;
         }
 
         /* Set ti_kick in the time structure*/

commit b1bfd74aa844f5a1ef78b3113b437229dc9684ea

Fix segfault introduced with 1b397cc9c616f1d080e0a9d3b864fb2c232d0172

libgadget/sfr_eff.c
@@ -980,7 +980,7 @@ static double get_sfr_factor_due_to_h2(int i, MyFloat * GradRho_mag, const doubl
     double tau_fmol;
     const double a2 = atime * atime;
     double zoverzsun = SPHP(i).Metallicity/METAL_YIELD;
-    double gradrho_mag = GradRho_mag[3*P[i].PI];
+    double gradrho_mag = GradRho_mag[P[i].PI];
     //message(4, "GradRho %g rho %g hsml %g i %d\n", gradrho_mag, SPHP(i).Density, P[i].Hsml, i);
     tau_fmol = ev_NH_from_GradRho(gradrho_mag,P[i].Hsml,SPHP(i).Density,1) /a2;
     tau_fmol *= (0.1 + zoverzsun);