MP-Gadget/MP-Gadget

commit d7cfdaa36882b71c228989655491892b522bd7e4

Reduce extra memory allocated for fof particle exchange

libgadget/fofpetaio.c
@@ -271,8 +271,7 @@ fof_distribute_particles(struct part_manager_type * halo_pman, struct slots_mana
                 atleast[type]++;
         }
     }
-    double FOFPartAllocFactor = (double) PartManager->MaxPart / PartManager->NumPart;
-    halo_pman->MaxPart = NpigLocal * FOFPartAllocFactor;
+    halo_pman->MaxPart = NpigLocal * 1.02;
     struct particle_data * halopart = (struct particle_data *) mymalloc("HaloParticle", sizeof(struct particle_data) * halo_pman->MaxPart);
     halo_pman->Base = halopart;
     halo_pman->NumPart = NpigLocal;
@@ -280,8 +279,10 @@ fof_distribute_particles(struct part_manager_type * halo_pman, struct slots_mana
     memcpy(halo_pman->CurrentParticleOffset, PartManager->CurrentParticleOffset, 3 * sizeof(PartManager->CurrentParticleOffset[0]));
 
     /* We leave extra space in the hope that we can avoid compacting slots in the fof exchange*/
-    for(i = 0; i < 6; i ++)
-        atleast[i]*= FOFPartAllocFactor;
+    const double FOFPartAllocFactor = (double) PartManager->MaxPart / PartManager->NumPart;
+    atleast[0] *= 1.02;
+    atleast[4]*= FOFPartAllocFactor;
+    atleast[5]*= FOFPartAllocFactor;
 
     slots_reserve(0, atleast, halo_sman);
 

commit 8bb37b3df7527ac36d0f502bc6496c2cc77c84ba

Allocate more memory during FOF particle exchange
The exchange setup seems to assume MaxPart somewhere.

Fixes: d7cfdaa36882b71c228989655491892b522bd7e4

libgadget/fofpetaio.c
@@ -289,7 +289,8 @@ fof_distribute_particles(struct part_manager_type * halo_pman, struct slots_mana
     const uint64_t task_origin_offset = PartManager->MaxPart + 1Lu;
     fof_find_target_task(pi, NpigLocal, task_origin_offset, Comm);
 
-    halo_pman->MaxPart = NpigLocal * 1.02;
+    const double FOFPartAllocFactor = (double) PartManager->MaxPart / PartManager->NumPart;
+    halo_pman->MaxPart = NpigLocal * FOFPartAllocFactor;
     struct particle_data * halopart = (struct particle_data *) mymalloc("HaloParticle", sizeof(struct particle_data) * halo_pman->MaxPart);
     halo_pman->Base = halopart;
     halo_pman->NumPart = NpigLocal;
@@ -302,8 +303,7 @@ fof_distribute_particles(struct part_manager_type * halo_pman, struct slots_mana
     myfree(pi);
 
     /* We leave extra space in the hope that we can avoid compacting slots in the fof exchange*/
-    const double FOFPartAllocFactor = (double) PartManager->MaxPart / PartManager->NumPart;
-    atleast[0] *= 1.02;
+    atleast[0] *= 1.05;
     atleast[4]*= FOFPartAllocFactor;
     atleast[5]*= FOFPartAllocFactor;
 