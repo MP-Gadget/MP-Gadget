MP-Gadget/MP-Gadget

commit e11a709f8ad708594f2118df6bc294cda8a462c1

Do not recompute the topleaf for inactive DM if we do not have to

libgadget/domain.c
@@ -271,11 +271,16 @@ void domain_maintain(DomainDecomp * ddecomp, struct DriftData * drift)
             real_drift_particle(&PartManager->Base[i], SlotsManager, ddrift, PartManager->BoxSize, rel_random_shift);
             PartManager->Base[i].Ti_drift = drift->ti1;
         }
-        if(!inside_topleaf(PartManager->Base[i].TopLeaf, PartManager->Base[i].Pos, &tree)) {
-            const int no = force_tree_find_topnode(PartManager->Base[i].Pos, &tree);
-            /* Set the topleaf for layoutfunc.*/
-            PartManager->Base[i].TopLeaf = tree.Nodes[no].s.suns[0] - tree.lastnode;
-        }
+        /* If we aren't using DM for the dynamic friction, we don't need to build a tree with inactive DM particles.
+         * Velocity dispersions are computed on a PM step only.*/
+        if(!(blackhole_dynfric_treemask() & DMMASK))
+            if(PartManager->Base[i].Type == 1 && !is_timebin_active(PartManager->Base[i].TimeBinGravity, drift->ti1))
+                continue;
+        if(inside_topleaf(PartManager->Base[i].TopLeaf, PartManager->Base[i].Pos, &tree))
+            continue;
+        const int no = force_tree_find_topnode(PartManager->Base[i].Pos, &tree);
+        /* Set the topleaf for layoutfunc.*/
+        PartManager->Base[i].TopLeaf = tree.Nodes[no].s.suns[0] - tree.lastnode;
     }
 
     walltime_measure("/Domain/drift");

commit af6da5d33634596ceb03d0fe87840deeb70ba0a9

Fix null dereference in domain when drift is NULL
Thanks to the clang static analyzer.

Fixes: e11a709f8ad708594f2118df6bc294cda8a462c1

libgadget/domain.c
@@ -273,7 +273,7 @@ void domain_maintain(DomainDecomp * ddecomp, struct DriftData * drift)
         /* If we aren't using DM for the dynamic friction, we don't need to build a tree with inactive DM particles.
          * Velocity dispersions are computed on a PM step only.*/
         if(!(blackhole_dynfric_treemask() & DMMASK))
-            if(PartManager->Base[i].Type == 1 && !is_timebin_active(PartManager->Base[i].TimeBinGravity, drift->ti1))
+            if(PartManager->Base[i].Type == 1 && !is_timebin_active(PartManager->Base[i].TimeBinGravity, PartManager->Base[i].Ti_drift))
                 continue;
         if(inside_topleaf(PartManager->Base[i].TopLeaf, PartManager->Base[i].Pos, &tree))
             continue;