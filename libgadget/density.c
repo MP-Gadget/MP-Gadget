d00c0b474e1ba48e106dd229e789333bd4291967
Simeon Bird <sbird@ucr.edu>
2024-01-28 20:50:31 -0800

    Adjust error condition

--- libgadget/density.c
+++ libgadget/density.c
@@ -550,9 +550,11 @@ density_postprocess(int i, TreeWalk * tw)
                 EntPred = DENSITY_GET_PRIV(tw)->SPH_predicted->EntVarPred[P[i].PI];
             else
                 EntPred = SPH_EntVarPred(i, DENSITY_GET_PRIV(tw)->times);
-            if(EntPred <= 0 || SPHP(i).EgyWtDensity <=0)
+            if(EntPred <= 0 || SPHP(i).EgyWtDensity < 0)
                 endrun(12, "Particle %d has bad predicted entropy: %g or EgyWtDensity: %g, Particle ID = %ld, pos %g %g %g, vel %g %g %g, mass = %g, density = %g, MaxSignalVel = %g, Entropy = %g, DtEntropy = %g \n", i, EntPred, SPHP(i).EgyWtDensity, P[i].ID, P[i].Pos[0], P[i].Pos[1], P[i].Pos[2], P[i].Vel[0], P[i].Vel[1], P[i].Vel[2], P[i].Mass, SPHP(i).Density, SPHP(i).MaxSignalVel, SPHP(i).Entropy, SPHP(i).DtEntropy);
-            SPHP(i).DhsmlEgyDensityFactor *= P[i].Hsml/ (NUMDIMS * SPHP(i).EgyWtDensity);
+            /* SPHP(i).EgyWtDensity may happen if NumNgb is zero because Hsml is very badly converged.*/
+            if(SPHP(i).EgyWtDensity > 0)
+                SPHP(i).DhsmlEgyDensityFactor *= P[i].Hsml/ (NUMDIMS * SPHP(i).EgyWtDensity);
             SPHP(i).DhsmlEgyDensityFactor *= - (*DhsmlDens);
             SPHP(i).EgyWtDensity /= EntPred;
         }
