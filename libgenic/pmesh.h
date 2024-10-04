8a338409ebb0a1691f4808cb661585715a1bfcfd
Yanhui Yang <yanhui.yang@email.ucr.edu>
2024-10-03 20:55:47 -0700

    pmesh rng boost mt

--- libgenic/pmesh.h
+++ libgenic/pmesh.h
@@ -1,13 +1,10 @@
 #ifndef PMESH_H
 #define PMESH_H
-#include <gsl/gsl_rng.h>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
 #include <libgadget/petapm.h>
 #include <libgadget/utils.h>
 
-/*
- * The following functions are from fastpm/libfastpm/initialcondition.c.
- * Agrees with nbodykit's pmesh/whitenoise.c, which agrees with n-genic.
- * */
 typedef struct {
     struct {
         ptrdiff_t start[3];
@@ -19,9 +16,10 @@ typedef struct {
 } PMDesc;
 
 static inline void
-SETSEED(PMDesc * pm, unsigned int * table[2][2], int i, int j, gsl_rng * rng)
+SETSEED(PMDesc * pm, unsigned int * table[2][2], int i, int j, boost::random::mt19937 & rng)
 {
-    unsigned int seed = 0x7fffffff * gsl_rng_uniform(rng);
+    boost::random::uniform_real_distribution<double> dist(0, 1);
+    unsigned int seed = static_cast<unsigned int>(0x7fffffff * dist(rng));
 
     int ii[2] = {i, (pm->Nmesh[0] - i) % pm->Nmesh[0]};
     int jj[2] = {j, (pm->Nmesh[1] - j) % pm->Nmesh[1]};
@@ -41,6 +39,7 @@ SETSEED(PMDesc * pm, unsigned int * table[2][2], int i, int j, gsl_rng * rng)
         }
     }
 }
+
 static inline unsigned int
 GETSEED(PMDesc * pm, unsigned int * table[2][2], int i, int j, int d1, int d2)
 {
@@ -54,11 +53,12 @@ GETSEED(PMDesc * pm, unsigned int * table[2][2], int i, int j, int d1, int d2)
 }
 
 static void
-SAMPLE(gsl_rng * rng, double * ampl, double * phase)
+SAMPLE(boost::random::mt19937 & rng, double * ampl, double * phase)
 {
-    *phase = gsl_rng_uniform(rng) * 2 * M_PI;
+    boost::random::uniform_real_distribution<double> dist(0, 1);
+    *phase = dist(rng) * 2 * M_PI;
     *ampl = 0;
-    do *ampl = gsl_rng_uniform(rng); while(*ampl == 0);
+    do *ampl = dist(rng); while(*ampl == 0);
 }
 
 static void
@@ -68,8 +68,8 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
     int d;
     int i, j, k;
 
-    gsl_rng * rng = gsl_rng_alloc(gsl_rng_ranlxd1);
-    gsl_rng_set(rng, seed);
+    // Initialize the Boost RNG
+    boost::random::mt19937 rng(seed);
 
     unsigned int * seedtable[2][2];
     for(i = 0; i < 2; i ++)
@@ -88,15 +88,13 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
         for(j = 0; j < i; j++) SETSEED(pm, seedtable, pm->Nmesh[0] - 1 - i, pm->Nmesh[1] - 1 - j, rng);
         for(j = 0; j < i + 1; j++) SETSEED(pm, seedtable, pm->Nmesh[1] - 1 - j, pm->Nmesh[0] - 1 - i, rng);
     }
-    gsl_rng_free(rng);
 
     ptrdiff_t irel[3];
     for(i = pm->ORegion.start[0];
         i < pm->ORegion.start[0] + pm->ORegion.size[0];
         i ++) {
 
-        gsl_rng * lower_rng = gsl_rng_alloc(gsl_rng_ranlxd1);
-        gsl_rng * this_rng = gsl_rng_alloc(gsl_rng_ranlxd1);
+        boost::random::mt19937 lower_rng, this_rng;
 
         int ci = pm->Nmesh[0] - i;
         if(ci >= pm->Nmesh[0]) ci -= pm->Nmesh[0];
@@ -104,15 +102,10 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
         for(j = pm->ORegion.start[1];
             j < pm->ORegion.start[1] + pm->ORegion.size[1];
             j ++) {
-            /* always pull the gaussian from the lower quadrant plane for k = 0
-             * plane*/
-            /* always pull the whitenoise from the lower quadrant plane for k = 0
-             * plane and k == All.Nmesh / 2 plane*/
             int d1 = 0, d2 = 0;
             int cj = pm->Nmesh[1] - j;
             if(cj >= pm->Nmesh[1]) cj -= pm->Nmesh[1];
 
-            /* d1, d2 points to the conjugate quandrant */
             if( (ci == i && cj < j)
              || (ci < i && cj != j)
              || (ci < i && cj == j)) {
@@ -121,20 +114,17 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
             }
 
             unsigned int seed_conj, seed_this;
-            /* the lower quadrant generator */
             seed_conj = GETSEED(pm, seedtable, i, j, d1, d2);
-            gsl_rng_set(lower_rng, seed_conj);
+            lower_rng.seed(seed_conj);
 
             seed_this = GETSEED(pm, seedtable, i, j, 0, 0);
-            gsl_rng_set(this_rng, seed_this);
+            this_rng.seed(seed_this);
 
             for(k = 0; k <= pm->Nmesh[2] / 2; k ++) {
                 int use_conj = (d1 != 0 || d2 != 0) && (k == 0 || k == pm->Nmesh[2] / 2);
 
                 double ampl, phase;
                 if(use_conj) {
-                    /* on k = 0 and All.Nmesh/2 plane, we use the lower quadrant generator,
-                     * then hermit transform the result if it is nessessary */
                     SAMPLE(this_rng, &ampl, &phase);
                     SAMPLE(lower_rng, &ampl, &phase);
                 } else {
@@ -152,14 +142,12 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
                 if(irel[2] < 0) continue;
                 if(irel[2] >= pm->ORegion.size[2]) continue;
 
-                /* we want two numbers that are of std ~ 1/sqrt(2) */
                 ampl = sqrt(- log(ampl));
 
-                if (setUnitaryAmplitude) ampl = 1.0; /* cos and sin gives 1/sqrt(2)*/
-
+                if (setUnitaryAmplitude) ampl = 1.0;
 
                 if (setInvertPhase){
-                  phase += M_PI; /*invert phase*/
+                  phase += M_PI;
                 }
 
                 (delta_k + 2 * ip)[0] = ampl * cos(phase);
@@ -172,29 +160,21 @@ pmic_fill_gaussian_gadget(PMDesc * pm, double * delta_k, int seed, int setUnitar
                 if((pm->Nmesh[0] - iabs[0]) % pm->Nmesh[0] == iabs[0] &&
                    (pm->Nmesh[1] - iabs[1]) % pm->Nmesh[1] == iabs[1] &&
                    (pm->Nmesh[2] - iabs[2]) % pm->Nmesh[2] == iabs[2]) {
-                    /* The mode is self conjuguate, thus imaginary mode must be zero */
                     (delta_k + 2 * ip)[1] = 0;
                     (delta_k + 2 * ip)[0] = ampl * cos(phase);
                 }
 
                 if(iabs[0] == 0 && iabs[1] == 0 && iabs[2] == 0) {
-                    /* the mean is zero */
                     (delta_k + 2 * ip)[0] = 0;
                     (delta_k + 2 * ip)[1] = 0;
                 }
             }
         }
-        gsl_rng_free(lower_rng);
-        gsl_rng_free(this_rng);
     }
     for(i = 1; i >= 0; i --)
     for(j = 1; j >= 0; j --) {
         myfree(seedtable[i][j]);
     }
-/*
-    char * fn[1000];
-    sprintf(fn, "canvas.dump.f4.%d", pm->ThisTask);
-    fwrite(pm->canvas, sizeof(pm->canvas[0]), pm->ORegion.total * 2, fopen(fn, "w"));
-*/
 }
+
 #endif
