b83542708555d8a810234929c93684b5d33e31d1
Simeon Bird <sbird@ucr.edu>
2024-10-04 21:51:45 -0700

    Remove largely unused lightcone

--- libgadget/lightcone.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIGHTCONE_H
-#define LIGHTCONE_H
-
-/* Initialise the lightcone code module. */
-void lightcone_init(Cosmology * CP, double timeBegin, const double UnitLength_in_cm, const char * OutputDir);
-void lightcone_compute(double a, double BoxSize, Cosmology * CP, inttime_t ti_curr, inttime_t ti_next, const RandTable * const rnd);
-#endif
