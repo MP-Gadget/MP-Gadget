0a85cb2ab594c17479d8f1ad0ed2c51036127180
Simeon Bird <sbird@ucr.edu>
2024-11-29 22:16:06 -0800

    Make the forcetree counting variables 64-bit

--- libgadget/treewalk.c
+++ libgadget/treewalk.c
@@ -328,7 +328,7 @@ int treewalk_export_particle(LocalTreeWalk * lv, int no)
     if(!lv->DataIndexTable)
         endrun(1, "DataIndexTable not allocated, treewalk_export_particle called in the wrong way\n");
     if(no - lv->tw->tree->lastnode > lv->tw->tree->NTopLeaves)
-        endrun(1, "Bad export leaf: no = %d lastnode %d ntop %d target %d\n", no, lv->tw->tree->lastnode, lv->tw->tree->NTopLeaves, lv->target);
+        endrun(1, "Bad export leaf: no = %d lastnode %ld ntop %d target %d\n", no, lv->tw->tree->lastnode, lv->tw->tree->NTopLeaves, lv->target);
     const int target = lv->target;
     TreeWalk * tw = lv->tw;
     const int task = tw->tree->TopLeaves[no - tw->tree->lastnode].Task;
