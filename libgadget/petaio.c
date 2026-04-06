b4a7f679e370194805135fe4b53e4292998a8815
Simeon Bird <sbird@ucr.edu>
2026-04-06 10:17:48 -0700

    Fix potential deadlock when writing, if the domain decomposition has no gas or DM entries on one rank

--- libgadget/petaio.c
+++ libgadget/petaio.c
@@ -191,7 +191,7 @@ petaio_save_snapshot(const char * fname, struct IOTable * IOTable, int verbose, 
         }
         /* No need to write empty folders for particle types we don't have.
          * But do still write them for stars and BHs as someone might expect them.*/
-        if(ptype_count[ptype] == 0 && ptype < 4)
+        if(NTotal[ptype] == 0 && ptype < 4)
             continue;
         sprintf(blockname, "%d/%s", ptype, IOTable->ent[i].name);
         petaio_build_buffer(&array, &IOTable->ent[i], selection + ptype_offset[ptype], ptype_count[ptype], P, SlotsManager, &conv);
